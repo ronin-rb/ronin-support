57223f1889c442eb1a63c7bf29efb9f25f9b5703
postmodern <postmodern.mod3@gmail.com>
2010-02-03 22:20:14 -0800

    Removed a spec.

--- spec/extensions/uri/http_spec.rb
+++ spec/extensions/uri/http_spec.rb
@@ -6,8 +6,4 @@ describe URI::HTTP do
   it "should include QueryParams" do
     URI::HTTP.should include(URI::QueryParams)
   end
-
-  it "should include Ronin::Scanners::Scanner" do
-    URI::HTTP.should include(Ronin::Scanners::Scanner)
-  end
 end
