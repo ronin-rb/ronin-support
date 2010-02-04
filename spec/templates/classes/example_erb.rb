8f72f538d1f865461d6c624bdce6a9688ba28c71
postmodern <postmodern.mod3@gmail.com>
2010-02-03 22:02:08 -0800

    Moved the Ronin::Templates code into the ronin-ext library.

---
+++ spec/templates/classes/example_erb.rb
@@ -0,0 +1,11 @@
+require 'ronin/templates/erb'
+
+class ExampleErb
+
+  include Ronin::Templates::Erb
+
+  attr_accessor :x
+
+  attr_accessor :y
+
+end
---
+++ spec/templates/helpers/static.rb
@@ -0,0 +1,7 @@
+require 'ronin/config'
+
+module Helpers
+  STATIC_TEMPLATE_DIR = File.expand_path(File.join(File.dirname(__FILE__),'static'))
+
+  Ronin::Config.register_static_dir STATIC_TEMPLATE_DIR
+end
