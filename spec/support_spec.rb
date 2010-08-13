2c398416941b9316f4caa79220d218b09345cfc6
Postmodern <postmodern.mod3@gmail.com>
2010-08-13 02:44:36 -0700

    More use of 'subject' in the specs.

--- spec/support_spec.rb
+++ spec/support_spec.rb
@@ -3,6 +3,6 @@ require 'ronin/support/version'
 
 describe Support do
   it "should have a version" do
-    Support.const_defined?('VERSION').should == true
+    subject.const_defined?('VERSION').should == true
   end
 end
