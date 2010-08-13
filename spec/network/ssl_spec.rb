d8c662de85d1e8f5573282e8fe6f8cd5fdd8237b
Postmodern <postmodern.mod3@gmail.com>
2010-08-13 01:47:52 -0700

    More RSpec 'subject' goodness.

--- spec/network/ssl_spec.rb
+++ spec/network/ssl_spec.rb
@@ -4,11 +4,11 @@ require 'ronin/network/ssl'
 describe Network::SSL do
   describe 'verify' do
     it "should map verify mode names to numeric values" do
-      Network::SSL.verify(:peer).should == OpenSSL::SSL::VERIFY_PEER
+      subject.verify(:peer).should == OpenSSL::SSL::VERIFY_PEER
     end
 
     it "should default to VERIFY_NONE if no verify mode name is given" do
-      Network::SSL.verify.should == OpenSSL::SSL::VERIFY_NONE
+      subject.verify.should == OpenSSL::SSL::VERIFY_NONE
     end
   end
 end
