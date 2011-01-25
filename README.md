431c8b71bb4e08d0f2e2af84cefe52bdf5cb4298
Postmodern <postmodern.mod3@gmail.com>
2011-01-25 15:57:42 -0800

    Fixed typo in license.

--- README.md
+++ README.md
@@ -61,8 +61,8 @@ Copyright (c) 2006-2011 Hal Brodigan (postmodern.mod3 at gmail.com)
 This file is part of Ronin Support.
 
 Ronin Support is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as publishe by
-the Free Software Foundation, either version 3 of the License, or
+it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
 Ronin Support is distributed in the hope that it will be useful,
--- lib/ronin/templates/erb.rb
+++ lib/ronin/templates/erb.rb
@@ -4,8 +4,8 @@
 # This file is part of Ronin Support.
 #
 # Ronin Support is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as publishe by
-# the Free Software Foundation, either version 3 of the License, or
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # Ronin Support is distributed in the hope that it will be useful,
@@ -25,7 +25,7 @@ module Ronin
   module Templates
     #
     # The {Erb} module uses the {Template} module to find and render
-    # Embedded Ruby (ERb) templates.
+    # Embedded Ruby (ERB) templates.
     #
     module Erb
       include Template
