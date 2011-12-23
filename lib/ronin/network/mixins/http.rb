b76edd438ab326ccea7eabf97325bda9c15efab2
Postmodern <postmodern.mod3@gmail.com>
2011-12-23 02:02:14 -0800

    Allow yielding the expanded_options from Mixins::HTTP#http_session.

--- lib/ronin/network/mixins/http.rb
+++ lib/ronin/network/mixins/http.rb
@@ -115,13 +115,17 @@ module Ronin
         #
         # @api public
         #
-        def http_session(options={})
+        def http_session(options={},&block)
           options = http_merge_options(options)
 
-          super(options) do |http|
+          super(options) do |http,expanded_options|
             print_info "Starting HTTP Session with #{host_port}"
 
-            yield http
+            if block.arity == 2
+              block.call(sess,expanded_options)
+            else
+              block.call(sess)
+            end
 
             print_info "Closing HTTP Session with #{host_port}"
           end
