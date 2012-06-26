5b6288a1ddf37bdb979ad5a0e2e9cdf5f726bfbc
Postmodern <postmodern.mod3@gmail.com>
2012-06-25 21:03:29 -0700

    Mark the Network.ip spec as network.

--- spec/network/network_spec.rb
+++ spec/network/network_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'ronin/network/network'
 
 describe Network do
-  it "should determine our public facing IP Address" do
+  it "should determine our public facing IP Address", :network do
     subject.ip.should_not be_nil
   end
 end
