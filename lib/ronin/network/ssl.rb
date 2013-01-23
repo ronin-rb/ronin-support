3f4332b6b50e88bc1f9bcb7bcd47c6673abfebed
Postmodern <postmodern.mod3@gmail.com>
2013-01-23 03:40:31 -0800

    Fixed an example.

--- lib/ronin/network/ssl.rb
+++ lib/ronin/network/ssl.rb
@@ -544,7 +544,7 @@ module Ronin
       #   # $ openssl req -new -key ssl.key -x509 -days 3653 -out ssl.crt
       #   # $ cat ssl.key ssl.crt > ssl.pem
       #   # $ chmod 600 ssl.key ssl.pem
-      #   ssl_server_loop(port: 1337, cert: 'ssl.crt', key: 'ssl.key') do |sock|
+      #   ssl_accept(port: 1337, cert: 'ssl.crt', key: 'ssl.key') do |client|
       #     client.puts 'lol'
       #   end
       #
