5b89655dc439c6d2d9a2b5e3a86566a99064b74e
Postmodern <postmodern.mod3@gmail.com>
2013-03-22 22:03:42 -0700

    Refactored the Ronin::Template::Erb specs.

--- spec/templates/erb_spec.rb
+++ spec/templates/erb_spec.rb
@@ -1,21 +1,39 @@
 require 'spec_helper'
 require 'ronin/templates/erb'
 
-require 'templates/classes/example_erb'
-
 describe Templates::Erb do
-  subject { ExampleErb.new }
+  class ErbTemplate
 
-  before(:all) do
-    subject.x = 2
-    subject.y = 3
+    include Ronin::Templates::Erb
+
+    attr_accessor :x, :y
+
+    def initialize
+      @x = 2
+      @y = 3
+    end
+
+    def template_method
+      "foo"
+    end
+
   end
+
+  subject { ErbTemplate.new }
 
   it "should render inline ERB templates" do
     subject.erb(%{<%= 'hello' %>}).should == 'hello'
   end
 
-  it "should render ERB templates using the binding of the object" do
-    subject.erb(%{<%= @x %> <%= @y %>}).should == '2 3'
+  context "when the template uses instance variables" do
+    it "should render ERB templates using the binding of the object" do
+      subject.erb(%{<%= @x %> <%= @y %>}).should == "#{subject.x} #{subject.y}"
+    end
+  end
+
+  context "when the template calls instance methods" do
+    it "should render ERB templates using the instance method" do
+      subject.erb(%{<%= template_method %>}).should == subject.template_method
+    end
   end
 end
