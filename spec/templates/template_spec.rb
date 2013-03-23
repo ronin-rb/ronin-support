08d984319eb18ce667383a0d95caafb46b2e864e
Postmodern <postmodern.mod3@gmail.com>
2013-03-22 22:24:03 -0700

    Embed the ExampleTemplate class into the rspec group.

--- spec/templates/template_spec.rb
+++ spec/templates/template_spec.rb
@@ -1,10 +1,43 @@
 require 'spec_helper'
 require 'ronin/templates/template'
 
-require 'templates/classes/example_template'
 require 'templates/helpers/data'
 
 describe Templates::Template do
+  class ExampleTemplate
+
+    include Ronin::Templates::Template
+
+    def enter_example_template(&block)
+      enter_template(File.join('templates','example.erb'),&block)
+    end
+
+    def enter_relative_template(&block)
+      enter_template(File.join('templates','example.erb')) do |path|
+        enter_template(File.join('..','includes','_relative.erb'),&block)
+      end
+    end
+
+    def enter_missing_template(&block)
+      enter_template('totally_missing.erb',&block)
+    end
+
+    def read_example_template(&block)
+      read_template(File.join('templates','example.erb'),&block)
+    end
+
+    def read_relative_template(&block)
+      read_template(File.join('templates','example.erb')) do |contents|
+        read_template(File.join('..','includes','_relative.erb'),&block)
+      end
+    end
+
+    def read_missing_template(&block)
+      read_template('totally_missing.erb',&block)
+    end
+
+  end
+  
   let(:example_template) { File.join(Helpers::TEMPLATE_DIR,'templates','example.erb') }
   let(:relative_template) { File.join(Helpers::TEMPLATE_DIR,'includes','_relative.erb') }
 
