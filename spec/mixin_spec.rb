89110f75d9b46976b732c7c166e2db1116c5c095
Postmodern <postmodern.mod3@gmail.com>
2013-03-23 18:19:55 -0700

    Refactored Ronin::Mixin specs.

--- spec/mixin_spec.rb
+++ spec/mixin_spec.rb
@@ -1,53 +1,53 @@
 require 'spec_helper'
 require 'ronin/mixin'
 
-module Mixins
-  module Test1
-  end
-
-  module Test2
-  end
-end
-
 describe Mixin do
-  subject do
-    Module.new do
-      include Mixin
-
-      mixin Mixins::Test1, Mixins::Test2
-      mixin { @var = 1 }
+  module Mixins
+    module Test1
     end
+
+    module Test2
+    end
+  end
+
+  module MixinModule
+    include Ronin::Mixin
+
+    mixin Mixins::Test1, Mixins::Test2
+    mixin { @var = 1 }
   end
 
   context "when included" do
-    before(:all) do
-      @base = Class.new
-      @base.send :include, subject
+    subject do
+      Class.new.tap do |base|
+        base.send :include, MixinModule
+      end
     end
 
     it "should include the mixed in modules" do
-      @base.should include(Mixins::Test1)
-      @base.should include(Mixins::Test2)
+      subject.should include(Mixins::Test1)
+      subject.should include(Mixins::Test2)
     end
 
     it "should evaluate the mixin block" do
-      @base.instance_variable_get("@var").should == 1
+      subject.instance_variable_get("@var").should == 1
     end
   end
 
   context "when extended" do
-    before(:all) do
-      @base = Object.new
-      @base.send :extend, subject
+    subject do
+      Class.new.tap do |base|
+        base.send :extend, MixinModule
+      end
     end
 
     it "should extend the mixed in modules" do
-      @base.should be_kind_of(Mixins::Test1)
-      @base.should be_kind_of(Mixins::Test2)
+      subject.should be_kind_of(Mixins::Test1)
+      subject.should be_kind_of(Mixins::Test2)
     end
 
     it "should evaluate the mixin block" do
-      @base.instance_variable_get("@var").should == 1
+      subject.instance_variable_get("@var").should == 1
     end
   end
 end
