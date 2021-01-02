091e6e22aa475ecc7a52a0f949ec93667adbfacb
Postmodern <postmodern.mod3@gmail.com>
2021-01-01 23:06:30 -0800

    treat_symbols_as_metadata_keys_with_true_values is deprecated.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,5 @@ require 'ronin/support/version'
 include Ronin
 
 RSpec.configure do |specs|
-  specs.treat_symbols_as_metadata_keys_with_true_values = true
   specs.filter_run_excluding :network
 end
