d4cfb0fc401ca886ce61c74d8f19c6bcc957ebe4
Postmodern <postmodern.mod3@gmail.com>
2022-02-03 10:13:33 -0800

    Added specs for the `URI::HTTP` core_ext methods (closes #154).

---
+++ spec/network/http/core_ext/uri/http_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+require 'ronin/support/network/http/core_ext/uri/http'
+
+describe URI::HTTP do
+  subject { URI('https://example.com/') }
+
+  it "must define a #status method" do
+    expect(subject.respond_to?(:status)).to be(true)
+  end
+
+  it "must define a #ok? method" do
+    expect(subject.respond_to?(:ok?)).to be(true)
+  end
+
+  context "integration tests", :network do
+    describe "#status" do
+      it "must request the HTTP stauts for the URI" do
+        expect(subject.status).to eq(200)
+      end
+    end
+
+    describe "#ok?", :network do
+      context "when the URI returns has a HTTP 200 response" do
+        it "must return true" do
+          expect(subject.ok?).to be(true)
+        end
+      end
+
+      context "when the URI does not return a HTTP 200 response" do
+        subject { URI('https://example.com/foo') }
+
+        it "must return false" do
+          expect(subject.ok?).to be(false)
+        end
+      end
+    end
+  end
+end
