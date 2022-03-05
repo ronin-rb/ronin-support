121fe769df926a3f622c1d293ee1c7b64d8b5402
Postmodern <postmodern.mod3@gmail.com>
2022-03-04 21:25:51 -0800

    Use the hexdump fixture files for the `String#unhexdump` specs.

--- spec/binary/hexdump/core_ext/string_spec.rb
+++ spec/binary/hexdump/core_ext/string_spec.rb
@@ -2,17 +2,22 @@ require 'spec_helper'
 require 'ronin/support/binary/hexdump/core_ext/string'
 
 describe String do
+  let(:fixtures_dir) { File.expand_path('../fixtures',__dir__)        }
+  let(:hexdump_path) { File.join(fixtures_dir,'hexdump_hex_byte.txt') }
+  let(:hexdump)      { File.read(hexdump_path) }
+
+  subject { described_class.new(hexdump) }
+
   it "should provide String#unhexdump" do
     expect(subject).to respond_to(:unhexdump)
   end
 
   describe "#unhexdump" do
-    subject { "00000000  23 20 52 6f 6e 69 6e 20  53 75 70 70 6f 72 74 0a  |# Ronin Support.|\n00000010\n" }
-
-    let(:raw) { "# Ronin Support\n" }
+    let(:binary_path)  { File.join(fixtures_dir,'ascii.bin') }
+    let(:raw_data)     { File.binread(binary_path) }
 
     it "should unhexdump a String" do
-      expect(subject.unhexdump).to eq(raw)
+      expect(subject.unhexdump).to eq(raw_data)
     end
   end
 end
