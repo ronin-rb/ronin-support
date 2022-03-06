8c93d021bee8f396202ba3bb05580595b842c848
Postmodern <postmodern.mod3@gmail.com>
2022-03-05 22:22:59 -0800

    Wording.

--- spec/support_spec.rb
+++ spec/support_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'ronin/support'
 
 describe Ronin::Support do
-  it "should have a version" do
+  it "must have a version" do
     expect(subject.const_defined?('VERSION')).to be(true)
   end
 end
