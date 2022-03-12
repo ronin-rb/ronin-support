561f54fa1b8201c25a8394c83d522f68275af44f
Postmodern <postmodern.mod3@gmail.com>
2022-03-11 18:35:46 -0800

    Added `Ronin::Support::Binary::Types::UInt64Type` (issue #75).

---
+++ spec/binary/types/uint64_type_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+require 'ronin/support/binary/types/uint64_type'
+
+describe Ronin::Support::Binary::Types::UInt64Type do
+  it { expect(described_class).to be < Ronin::Support::Binary::Types::UIntType }
+
+  let(:endian)      { :little }
+  let(:pack_string) { 'Q<' }
+
+  subject do
+    described_class.new(
+      endian:      endian,
+      pack_string: pack_string
+    )
+  end
+
+  describe "#initialize" do
+    it "must default #size to 8" do
+      expect(subject.size).to eq(8)
+    end
+
+    it "must set #endian" do
+      expect(subject.endian).to eq(endian)
+    end
+
+    it "must default #signed to false" do
+      expect(subject.signed).to be(false)
+    end
+
+    it "must set #pack_string" do
+      expect(subject.pack_string).to eq(pack_string)
+    end
+
+    context "when the endian: keyword is not given" do
+      it do
+        expect {
+          described_class.new(
+            pack_string: pack_string
+          )
+        }.to raise_error(ArgumentError)
+      end
+    end
+
+    context "when the pack_string: keyword is not given" do
+      it do
+        expect {
+          described_class.new(
+            endian: endian
+          )
+        }.to raise_error(ArgumentError)
+      end
+    end
+  end
+end
