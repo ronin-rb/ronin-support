43e3306925cc8bf351acbc56dab3b1d8e0a00799
Postmodern <postmodern.mod3@gmail.com>
2022-03-11 17:26:00 -0800

    Added `Ronin::Support::Binary::Types::ArrayType` (issue #75).

---
+++ lib/ronin/support/binary/types/array_type.rb
@@ -0,0 +1,129 @@
+#
+# Copyright (c) 2006-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of ronin-support.
+#
+# ronin-support is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-support is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-support.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+module Ronin
+  module Support
+    module Binary
+      module Types
+        #
+        # Represents a bounded array type.
+        #
+        # @api private
+        #
+        # @since 1.0.0
+        #
+        class ArrayType
+
+          # The type of each element in the array type.
+          #
+          # @return [Type]
+          attr_reader :type
+
+          # The length of the array type.
+          #
+          # @return [Integer]
+          attr_reader :length
+
+          # The size of the array type.
+          #
+          # @return [Integer]
+          attr_reader :size
+
+          # The `Array#pack`/`String#unpack` string for the array type.
+          #
+          # @return [String]
+          attr_reader :pack_string
+
+          #
+          # Initializes the array type.
+          #
+          # @param [Type] type
+          #   The type of each element in the array type.
+          #
+          # @param [Integer] length
+          #   The length of the array type.
+          #
+          def initialize(type,length)
+            @type   = type
+            @length = length
+            @size   = @type.size * @length
+
+            @pack_string = "#{@type.pack_string}#{@length}"
+          end
+
+          #
+          # The endianness of each element in the bounded array type.
+          #
+          # @return [:little, :big, nil]
+          #   Indicates whether each element is little-endian, big-endian,
+          #   or `nil` if each element has no endianness.
+          #
+          def endian
+            @type.endian
+          end
+
+          #
+          # Indicates whether each element is signed.
+          #
+          # @return [Boolean]
+          #
+          def signed?
+            @type.signed?
+          end
+
+          #
+          # Indicates whether each element is unsigned.
+          #
+          # @return [Boolean]
+          #
+          def unsigned?
+            @type.unsigned?
+          end
+
+          #
+          # Packs multiple values into binary data.
+          #
+          # @param [Array<Integer, Float, String>] values
+          #   The values to be packed.
+          #
+          # @return [String]
+          #   The packed binary data.
+          #
+          def pack(*values)
+            values.pack(@pack_string)
+          end
+
+          #
+          # Unpacks binary data.
+          #
+          # @param [String] data
+          #   The binary data to unpack.
+          #
+          # @return [Array<Integer, Float, String>]
+          #   The unpacked values.
+          #
+          def unpack(data)
+            data.unpack(@pack_string)
+          end
+
+        end
+      end
+    end
+  end
+end
---
+++ spec/binary/types/array_type_spec.rb
@@ -0,0 +1,87 @@
+require 'spec_helper'
+require 'ronin/support/binary/types/array_type'
+require 'ronin/support/binary/types/type'
+
+describe Ronin::Support::Binary::Types::ArrayType do
+  let(:size)        { 4       }
+  let(:endian)      { :little }
+  let(:signed)      { true    }
+  let(:pack_string) { 'L<'    }
+
+  let(:type) do
+    Ronin::Support::Binary::Types::Type.new(
+      size:        size,
+      endian:      endian,
+      signed:      signed,
+      pack_string: pack_string
+    )
+  end
+
+  let(:length) { 10 }
+  subject { described_class.new(type,length) }
+
+  describe "#initialize" do
+    it "must set #type" do
+      expect(subject.type).to eq(type)
+    end
+
+    it "must set #length" do
+      expect(subject.length).to eq(length)
+    end
+
+    it "must set #size as #type.size * length" do
+      expect(subject.size).to eq(type.size * length)
+    end
+
+    it "must set #pack_string to '\#{type.pack_string}\#{length}'" do
+      expect(subject.pack_string).to eq("#{type.pack_string}#{length}")
+    end
+  end
+
+  describe "#endian" do
+    it "must return #type.endian" do
+      expect(subject.endian).to eq(type.endian)
+    end
+  end
+
+  describe "#signed?" do
+    it "must return #type.signed?" do
+      expect(subject.signed?).to eq(type.signed?)
+    end
+  end
+
+  describe "#unsigned?" do
+    it "must return #type.unsigned?" do
+      expect(subject.unsigned?).to eq(type.unsigned?)
+    end
+  end
+
+  describe "#pack" do
+    let(:values) { (1..10).to_a }
+
+    it "must pack multiple values using Array#pack and #pack_string" do
+      expect(subject.pack(*values)).to eq(values.pack(subject.pack_string))
+    end
+  end
+
+  describe "#unpack" do
+    let(:data) do
+      [
+        "\x01\x00\x00\x00",
+        "\x02\x00\x00\x00",
+        "\x03\x00\x00\x00",
+        "\x04\x00\x00\x00",
+        "\x05\x00\x00\x00",
+        "\x06\x00\x00\x00",
+        "\x07\x00\x00\x00",
+        "\x08\x00\x00\x00",
+        "\x09\x00\x00\x00",
+        "\x0a\x00\x00\x00"
+      ].join
+    end
+
+    it "must unpack multiple values using String#unpack and #pack_string" do
+      expect(subject.unpack(data)).to eq(data.unpack(subject.pack_string))
+    end
+  end
+end
