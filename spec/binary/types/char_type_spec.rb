46329c627cccda6e5bf5b9575bbf5cabd318d77c
Postmodern <postmodern.mod3@gmail.com>
2022-03-11 18:24:35 -0800

    Added `Ronin::Support::Binary::Types::CharType` (issue #75).

---
+++ spec/binary/types/char_type_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+require 'ronin/support/binary/types/char_type'
+
+describe Ronin::Support::Binary::Types::CharType do
+  it { expect(described_class).to be < Ronin::Support::Binary::Types::Type }
+
+  let(:size)        { 4       }
+  let(:signed)      { true    }
+  let(:pack_string) { 'Z'     }
+
+  subject do
+    described_class.new(
+      size:        size,
+      signed:      signed,
+      pack_string: pack_string
+    )
+  end
+
+  describe "#initialize" do
+    it "must set #size" do
+      expect(subject.size).to eq(size)
+    end
+
+    it "must default #endian to nil" do
+      expect(subject.endian).to be(nil)
+    end
+
+    it "must set #signed" do
+      expect(subject.signed).to be(signed)
+    end
+
+    it "must set #pack_string" do
+      expect(subject.pack_string).to eq(pack_string)
+    end
+
+    context "when the signed: keyword is not given" do
+      it do
+        expect {
+          described_class.new(
+            pack_string: pack_string
+          )
+        }.to raise_error(ArgumentError)
+      end
+    end
+
+    context "when the pack_string: keyword is not given" do
+      it do
+        expect {
+          described_class.new(
+            signed: signed
+          )
+        }.to raise_error(ArgumentError)
+      end
+    end
+  end
+end
