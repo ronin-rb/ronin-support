fd65ae8f5d54dbf1343fcb4b4dcaeddc24daf3b4
Postmodern <postmodern.mod3@gmail.com>
2022-03-18 01:44:27 -0700

    Fixed a spec.

--- spec/binary/types/string_type_spec.rb
+++ spec/binary/types/string_type_spec.rb
@@ -3,6 +3,6 @@ require 'ronin/support/binary/types/string_type'
 
 describe Ronin::Support::Binary::Types::StringType do
   it do
-    expect(described_class).to be < Ronin::Support::Binary::Types::UnboundedArray
+    expect(described_class).to be < Ronin::Support::Binary::Types::UnboundedArrayType
   end
 end
