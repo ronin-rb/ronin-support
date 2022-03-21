b7b62022c1f87455b95ffa7b9e81643c4c9366bd
Postmodern <postmodern.mod3@gmail.com>
2022-03-20 20:02:18 -0700

    Added `Ronin::Support::Binary::Stack` (closes #184).

---
+++ lib/ronin/support/binary/stack.rb
@@ -0,0 +1,245 @@
+#
+# Copyright (c) 2006-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of ronin-support.
+#
+# ronin-support is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-support is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-support.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/support/binary/types'
+
+module Ronin
+  module Support
+    module Binary
+      #
+      # Represents a stack that can have binary data pushed to or popped from
+      # it.
+      #
+      # ## Examples
+      #
+      # Creating a new stack:
+      #
+      #     stack = Stack.new
+      #     # => #<Ronin::Support::Binary::Stack: "">
+      #     stack.push 10
+      #     # => #<Ronin::Support::Binary::Stack: "\n\x00\x00\x00\x00\x00\x00\x00">
+      #     stack.push 0x41414141
+      #     # => #<Ronin::Support::Binary::Stack: "\n\x00\x00\x00\x00\x00\x00\x00AAAA\x00\x00\x00\x00">
+      #     stack.psuh -1
+      #     # => #<Ronin::Support::Binary::Stack: "\n\x00\x00\x00\x00\x00\x00\x00AAAA\x00\x00\x00\x00\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF">
+      #
+      # Creating a stack from an existing String:
+      #
+      #     stack = Stack.new("\x41\x00\x00\x00\x00\x00\x00\x00\x42\x00\x00\x00\x00\x00\x00\x00")
+      #     stack[0]
+      #     # => 65
+      #     stack[1]
+      #     # => 66
+      #     stack.pop
+      #     # => 66
+      #     stack.pop
+      #     # => 65
+      #
+      # @api public
+      #
+      # @since 1.0.0
+      #
+      class Stack
+
+        # The endianness of data within the array buffer.
+        #
+        # @return [:little, :big, :net, nil]
+        attr_reader :endian
+
+        # The desired architecture for the array buffer.
+        #
+        # @return [Symbol, nil]
+        attr_reader :arch
+
+        # The type system that the buffer is using.
+        #
+        # @return [Types, Types::LittleEndian,
+        #                 Types::BigEndian,
+        #                 Types::Network]
+        attr_reader :type_system
+
+        # The "machine word" from the {#type_system}.
+        #
+        # @return [Types::UInt32Type, Types::UInt64Type]
+        attr_reader :machine_word
+
+        # The number of machine words on the stack.
+        #
+        # @return [Integer]
+        attr_reader :length
+
+        # The size of the stack in bytes.
+        #
+        # @return [Integer]
+        attr_reader :size
+
+        # The underlying buffer for the stack.
+        #
+        # @return [String]
+        attr_reader :string
+
+        #
+        # Initializes the stack.
+        #
+        # @param [String, nil] string
+        #   Optional existing stack contents.
+        #
+        # @param [:little, :big, :net, nil] endian
+        #   The desired endianness of the values within the buffer.
+        #
+        # @param [:x86, :x86_64,
+        #         :ppc, :ppc64,
+        #         :mips, :mips_le, :mips_be,
+        #         :mips64, :mips64_le, :mips64_be,
+        #         :arm, :arm_le, :arm_be,
+        #         :arm64, :arm64_le, :arm64_be] arch
+        #   The desired architecture for the values within the buffer.
+        #
+        def initialize(string=nil, endian: nil, arch: nil)
+          @endian = endian
+          @arch   = arch
+
+          @type_system = if arch then Types.arch(arch)
+                         else         Types.endian(endian)
+                         end
+
+          @machine_word = @type_system::MACHINE_WORD
+
+          if string
+            @string = string
+            @size   = @string.bytesize
+            @length = @size / @machine_word.size
+          else
+            @string = String.new("", encoding: Encoding::ASCII_8BIT)
+            @size   = 0
+            @length = 0
+          end
+        end
+
+        #
+        # Accesses a machine word at the given index within the stack.
+        #
+        # @param [Integer] index
+        #   The index within the stack to access.
+        #
+        # @return [Integer]
+        #   The value at the index within the stack.
+        #
+        # @raise [IndexError]
+        #
+        def [](index)
+          offset = index * @machine_word.size
+
+          if offset+@machine_word.size > @size
+            raise(IndexError,"index #{index} is out of bounds: 0...#{@length}")
+          end
+
+          slice = @string[offset,@machine_word.size]
+
+          return @machine_word.unpack(slice)
+        end
+
+        #
+        # Sets a machine word at the given index within the stack.
+        #
+        # @param [Integer] index
+        #   The index within the stack.
+        #
+        # @param [Integer] value
+        #   The value to write at the index within the stack.
+        #
+        # @return [Integer]
+        #   The written value.
+        #
+        # @raise [IndexError]
+        #
+        def []=(index,value)
+          offset = index * @machine_word.size
+
+          if offset+@machine_word.size > @size
+            raise(IndexError,"index #{index} is out of bounds: 0...#{@length}")
+          end
+
+          data = @machine_word.pack(value)
+
+          @string[offset,@machine_word.size] = data
+          return value
+        end
+
+        #
+        # Pushes a value onto the top of the stack (or to the end of the
+        # underlying buffer).
+        #
+        # @param [Integer] value
+        #   The value to push onto the stack.
+        #
+        # @return [self]
+        #
+        def push(value)
+          data = @machine_word.pack(value)
+
+          @string << data
+          @length += 1
+          @size   += @machine_word.size
+          return self
+        end
+
+        #
+        # Pops a value off the top of the stack (or to the end of the underlying
+        # buffer).
+        #
+        # @return [Integer]
+        #   The value popped from the stack.
+        #
+        def pop
+          slice = @string[-@machine_word.size..]
+          value = @machine_word.unpack(slice)
+
+          @length -= 1
+          @size   -= @machine_word.size
+
+          @string = @string.byteslice(0,@size)
+          return value
+        end
+
+        #
+        # Cnnverts the stack into a String.
+        #
+        # @return [String]
+        #   The underlying buffer for the stack.
+        #
+        def to_s
+          @string
+        end
+
+        alias to_str to_s
+
+        #
+        # Inspects the stack object.
+        #
+        # @return [String]
+        #
+        def inspect
+          "#<#{self.class}: #{@string.inspect}>"
+        end
+
+      end
+    end
+  end
+end
---
+++ spec/binary/stack_spec.rb
@@ -0,0 +1,266 @@
+require 'spec_helper'
+require 'ronin/support/binary/stack'
+
+describe Ronin::Support::Binary::Stack do
+  describe "#initialize" do
+    subject { described_class.new }
+
+    it "must default #endian to nil" do
+      expect(subject.endian).to be(nil)
+    end
+
+    it "must default #arch to nil" do
+      expect(subject.arch).to be(nil)
+    end
+
+    it "must default #type_system to Ronin::Support::Binary::Types" do
+      expect(subject.type_system).to be(Ronin::Support::Binary::Types)
+    end
+
+    context "when the endian: keyword argument is given" do
+      let(:endian) { :little }
+
+      subject { described_class.new(endian: endian) }
+
+      it "must set #endian" do
+        expect(subject.endian).to be(endian)
+      end
+
+      it "must set #type_system to the Ronin::Support::Binary::Types:: module" do
+        expect(subject.type_system).to be(Ronin::Support::Binary::Types.endian(endian))
+      end
+    end
+
+    context "when the arch: keyword argument is given" do
+      let(:arch) { :x86 }
+
+      subject { described_class.new(arch: arch) }
+
+      it "must set #arch" do
+        expect(subject.arch).to be(arch)
+      end
+
+      it "must set #type_system to the Ronin::Support::Binary::Types::Arch:: module" do
+        expect(subject.type_system).to be(Ronin::Support::Binary::Types::Arch[arch])
+      end
+    end
+
+    context "when a String argument is given instead of a length argument" do
+      let(:length) { 8 }
+      let(:string) { "A" * length }
+
+      subject { described_class.new(string) }
+
+      it "must set #string to the given string value" do
+        expect(subject.string).to be(string)
+      end
+
+      it "must set #size to the string's byte size" do
+        expect(subject.size).to eq(string.bytesize)
+      end
+
+      it "must calculate #length by dividing #size by #machine_word.size" do
+        expect(subject.length).to eq(subject.size / subject.machine_word.size)
+      end
+    end
+  end
+
+  describe "#[]" do
+    context "when the stack is empty" do
+      let(:index) { 0 }
+
+      it do
+        expect {
+          subject[0]
+        }.to raise_error(IndexError,"index #{index} is out of bounds: 0...0")
+      end
+    end
+
+    context "when the stack is populated" do
+      let(:value1) { 1          }
+      let(:value2) { 0x41       }
+      let(:value3) { 0x11223344 }
+
+      before do
+        subject.push(value1)
+        subject.push(value2)
+        subject.push(value3)
+      end
+
+      it "must allow reading arbitrary values from the stack" do
+        expect(subject[1]).to eq(value2)
+      end
+
+      context "when the index of -1 is given" do
+        it "must read the value at the end of the buffer" do
+          expect(subject[-1]).to eq(value3)
+        end
+      end
+
+      context "when a negative index is given" do
+        it "must read the value relative to the end of the buffer" do
+          expect(subject[-2]).to eq(value2)
+        end
+      end
+    end
+  end
+
+  describe "#[]=" do
+    context "when the stack is empty" do
+      let(:index) { 0 }
+      let(:value) { 1 }
+
+      it do
+        expect {
+          subject[0] = value
+        }.to raise_error(IndexError,"index #{index} is out of bounds: 0...0")
+      end
+    end
+
+    context "when the stack is populated" do
+      let(:value1) { 1          }
+      let(:value2) { 0x41       }
+      let(:value3) { 0x11223344 }
+
+      let(:index)     { 1    }
+      let(:new_value) { 0x42 }
+
+      before do
+        subject.push(value1)
+        subject.push(value2)
+        subject.push(value3)
+
+        subject[index] = new_value
+      end
+
+      let(:machine_word) { subject.machine_word }
+      let(:offset)       { index * machine_word.size }
+      let(:packed_value) { machine_word.pack(new_value) }
+
+      it "must write the new value into the buffer at the offset" do
+        expect(subject.string[offset,machine_word.size]).to eq(packed_value)
+      end
+
+      context "when the index of -1 is given" do
+        let(:index)  { -1 }
+        let(:offset) { subject.size - machine_word.size }
+
+        it "must write the value at the end of the buffer" do
+          expect(subject.string[offset,machine_word.size]).to eq(packed_value)
+        end
+      end
+
+      context "when a negative index is given" do
+        let(:index)  { -2 }
+        let(:offset) { subject.size - (machine_word.size * -index) }
+
+        it "must write the value relative to the end of the buffer" do
+          expect(subject.string[offset,machine_word.size]).to eq(packed_value)
+        end
+      end
+    end
+  end
+
+  describe "#push" do
+    let(:value) { 0x11223344 }
+
+    let(:machine_word) { subject.machine_word     }
+    let(:packed_value) { machine_word.pack(value) }
+
+    before { subject.push(value) }
+
+    it "must write a machine word to the end of the buffer" do
+      expect(subject.string[-machine_word.size..]).to eq(packed_value)
+    end
+  end
+
+  describe "#pop" do
+    let(:value) { 0x11223344 }
+
+    let(:machine_word) { subject.machine_word     }
+    let(:packed_value) { machine_word.pack(value) }
+
+    context "when there is only one value on the stack" do
+      before { subject.push(value) }
+
+      it "must return the only value on the stack" do
+        expect(subject.pop).to eq(value)
+      end
+
+      it "must make #string empty" do
+        subject.pop
+
+        expect(subject.string).to eq('')
+      end
+    end
+
+    context "when there is multiple values on the stack" do
+      let(:last_value) { 0x11223344 }
+      let(:values)     { [1, 0x41414141, last_value] }
+
+      before do
+        values.each do |value|
+          subject.push(value)
+        end
+      end
+
+      it "must return the last value pushed onto the stack" do
+        expect(subject.pop).to eq(last_value)
+      end
+
+      it "must truncate the end of the buffer by #machine_word.size bytes" do
+        subject.pop
+
+        expect(subject.string.bytesize).to eq(
+          (values.length * machine_word.size) - machine_word.size
+        )
+      end
+    end
+  end
+
+  describe "#to_s" do
+    context "when the stack is empty" do
+      it "must return an empty String" do
+        expect(subject.to_s).to eq('')
+      end
+    end
+
+    context "when the stack is populated" do
+      before do
+        subject.push 1
+        subject.push 2
+        subject.push 3
+      end
+
+      it "must return #string" do
+        expect(subject.to_s).to be(subject.string)
+      end
+    end
+  end
+
+  describe "#to_str" do
+    context "when the stack is empty" do
+      it "must return an empty String" do
+        expect(subject.to_s).to eq('')
+      end
+    end
+
+    context "when the stack is populated" do
+      before do
+        subject.push 1
+        subject.push 2
+        subject.push 3
+      end
+
+      it "must return #string" do
+        expect(subject.to_s).to be(subject.string)
+      end
+    end
+  end
+
+  describe "#inspect" do
+    it "must return the class name and the buffer string" do
+      expect(subject.inspect).to eq("#<#{described_class}: #{subject.string.inspect}>")
+    end
+  end
+end
