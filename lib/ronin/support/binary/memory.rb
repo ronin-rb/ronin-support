da71d883b6e6d45d4286271bc4e8f7dc00bb76ef
Postmodern <postmodern.mod3@gmail.com>
2022-04-02 00:53:19 -0700

    Added `Binary::Memory` base class (closes #189).

---
+++ lib/ronin/support/binary/memory.rb
@@ -0,0 +1,83 @@
+#
+# Copyright (c) 2006-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of ronin-support.
+#
+# ronin-support is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-support is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-support.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/support/binary/types'
+require 'ronin/support/binary/byte_slice'
+
+module Ronin
+  module Support
+    module Binary
+      #
+      # Base class for all memory objects.
+      #
+      # @api private
+      #
+      # @since 1.0.0
+      #
+      class Memory
+
+        # The size of the memory in bytes.
+        #
+        # @return [Integer]
+        attr_reader :size
+
+        # The underlying String buffer.
+        #
+        # @return [String]
+        attr_reader :string
+
+        #
+        # Initializes the memory.
+        #
+        # @param [Integer, String, ByteSlice] size_or_string
+        #   The size of the buffer or an existing String which will be used
+        #   as the underlying buffer.
+        #
+        # @raise [ArgumentError]
+        #   The argument was not an Integer, String, or {ByteSlice}.
+        #
+        def initialize(size_or_string)
+          case size_or_string
+          when String, ByteSlice
+            @string = size_or_string
+            @size   = @string.bytesize
+          when Integer
+            @size   = size_or_string
+            @string = String.new("\0" * @size, encoding: Encoding::ASCII_8BIT)
+          else
+            raise(ArgumentError,"size_or_string argument must be either a size (Integer) or a buffer (String): #{size_or_string.inspect}")
+          end
+        end
+
+        #
+        # Converts the buffer to a String.
+        #
+        # @return [String]
+        #   The raw binary buffer.
+        #
+        def to_s
+          @string.to_s
+        end
+
+        alias to_str to_s
+
+      end
+    end
+  end
+end
---
+++ spec/binary/memory_spec.rb
@@ -0,0 +1,123 @@
+require 'spec_helper'
+require 'ronin/support/binary/memory'
+
+describe Ronin::Support::Binary::Memory do
+  let(:size) { 10 }
+
+  describe "#initialize" do
+    subject { described_class.new(size) }
+
+    context "when an Integer argument is given instead of a String argument" do
+      subject { described_class.new(size) }
+
+      it "must set #size" do
+        expect(subject.size).to eq(size)
+      end
+
+      it "must initialize #string with Encoding::ASCII_8BIT" do
+        expect(subject.string.encoding).to be(Encoding::ASCII_8BIT)
+      end
+
+      it "must initialize #string to the given size" do
+        expect(subject.string.bytesize).to eq(size)
+      end
+
+      it "must initialize #string to all null-bytes" do
+        expect(subject.string).to eq("\0" * size)
+      end
+    end
+
+    context "when a String argument is given instead of a size argument" do
+      let(:string) { "A" * size }
+
+      subject { described_class.new(string) }
+
+      it "must set #string to the given string value" do
+        expect(subject.string).to be(string)
+      end
+
+      it "must set #size to the string's byte size" do
+        expect(subject.size).to eq(string.bytesize)
+      end
+    end
+
+    context "when a Ronin::Support::Binary::ByteSlice is given instead of a String" do
+      let(:offset) { 4 }
+      let(:length) { 10 }
+      let(:string) { ('A' * offset) + ("B" * length) + ('C' * offset) }
+
+      let(:byte_slice) do
+        Ronin::Support::Binary::ByteSlice.new(string, offset: offset,
+                                                      length: length)
+      end
+
+      subject { described_class.new(byte_slice) }
+
+      it "must set #string to the ByteSlice object" do
+        expect(subject.string).to be(byte_slice)
+      end
+
+      it "must set #size to the ByteSlice's size in bytes" do
+        expect(subject.size).to eq(byte_slice.bytesize)
+      end
+    end
+
+    context "when not given a size or a string argument" do
+      let(:arg) { :foo }
+
+      it do
+        expect {
+          described_class.new(arg)
+        }.to raise_error(ArgumentError,"size_or_string argument must be either a size (Integer) or a buffer (String): #{arg.inspect}")
+      end
+    end
+  end
+
+  let(:string) { "foo bar baz" }
+
+  describe "#to_s" do
+    context "when #string is a String" do
+      subject { described_class.new(string) }
+
+      it "must return #string" do
+        expect(subject.to_s).to eq(subject.string)
+      end
+    end
+
+    context "when #string is a Ronin::Support::Binary::ByteSlice" do
+      let(:byte_slice) do
+        Ronin::Support::Binary::ByteSlice.new(string, offset: 4,
+                                                      length: 3)
+      end
+
+      subject { described_class.new(byte_slice) }
+
+      it "must return #string as a String" do
+        expect(subject.to_s).to eq(byte_slice.to_s)
+      end
+    end
+  end
+
+  describe "#to_str" do
+    context "when #string is a String" do
+      subject { described_class.new(string) }
+
+      it "must return #string" do
+        expect(subject.to_str).to eq(subject.string)
+      end
+    end
+
+    context "when #string is a Ronin::Support::Binary::ByteSlice" do
+      let(:byte_slice) do
+        Ronin::Support::Binary::ByteSlice.new(string, offset: 4,
+                                                      length: 3)
+      end
+
+      subject { described_class.new(byte_slice) }
+
+      it "must return #string as a String" do
+        expect(subject.to_str).to eq(byte_slice.to_s)
+      end
+    end
+  end
+end
