50e3f92956d4a020ff798cb4291604cd893a8431
Postmodern <postmodern.mod3@gmail.com>
2022-04-25 13:50:03 -0700

    Added `Binary::Types::Resolver`.

---
+++ lib/ronin/support/binary/types/resolver.rb
@@ -0,0 +1,147 @@
+#
+# Copyright (c) 2006-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of ronin-support.
+#
+# ronin-support is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-support is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-support.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/support/binary/types/type'
+require 'ronin/support/binary/types/array_object_type'
+require 'ronin/support/binary/types/struct_object_type'
+
+module Ronin
+  module Support
+    module Binary
+      module Types
+        #
+        # Adds a type system that can be used by {Memory} objects.
+        #
+        # @api private
+        #
+        class Resolver
+
+          # The {Types} module or object to lookup type names in.
+          #
+          # @return [#[]]
+          attr_reader :types
+
+          #
+          # Initializes the type resolver.
+          #
+          # @return [#[]] types
+          #   The types module or object that provides a `[]` method for
+          #   looking up type names.
+          #
+          def initialize(types)
+            @types = types
+          end
+
+          #
+          # Translates C type names into an Array of {Types} objects.
+          #
+          # @param [Symbol,
+          #         (type, Integer),
+          #         Range(type),
+          #         Binary::Struct.class] type_signature
+          #   The C type value. The value can be one of the following:
+          #   * `Symbol` - represents a single type (ex: `:int32`)
+          #   * `(type, Integer)` - represents an Array type with the given
+          #     element type and length (ex: `[:int32, 10]`)
+          #   * `Range(type)` - represents an unbounded Array type with the
+          #     given element type. (ex: `:int32..`)
+          #   * `Struct.class` - a subclass of {Binary::Struct}.
+          #
+          # @return [Type]
+          #   The translated type.
+          #
+          # @raise [ArgumentError]
+          #   The given type name was not known or not a `Symbol`,
+          #   `[Symbol, Integer]`, `Symbol..`, or a {Type}.
+          #
+          def resolve(type_signature)
+            case type_signature
+            when ::Array then resolve_array(type_signature)
+            when Range   then resolve_range(type_signature)
+            when Symbol  then resolve_symbol(type_signature)
+            when Type    then type_signature
+            when ->(type){ type.is_a?(Class) && type < Binary::Struct }
+              resolve_struct(type_signature)
+            else
+              raise(ArgumentError,"type type_signature must be a Symbol, Array, Range, #{Binary::Struct}, or a #{Types::Type} object: #{type_signature.inspect}")
+            end
+          end
+
+          private
+
+          #
+          # Resolves an array type.
+          #
+          # @param [(Symbol, Integer),
+          #         (Struct.class, Integer),
+          #         (Array, Integer)] type_signature
+          #
+          # @return [ArrayObjectType]
+          #
+          def resolve_array(type_signature)
+            type, length = *type_signature
+
+            ArrayObjectType.new(resolve(type), length)
+          end
+
+          #
+          # Resolves a range type (aka unbounded array).
+          #
+          # @param [Range] type_signature
+          #
+          # @return [UnboundedArrayType]
+          #
+          def resolve_range(type_signature)
+            range = type_signature
+            type  = resolve(range.begin)
+            type[]
+          end
+
+          #
+          # Resolves a struct type.
+          #
+          # @param [Struct.class] type_signature
+          #
+          # @return [StructObjectType]
+          #
+          def resolve_struct(type_signature)
+            struct_class   = type_signature
+            struct_members = Hash[struct_class.members.map { |name,member|
+              [name, resolve(member.type_signature)]
+            }]
+
+            StructObjectType.new(struct_class,struct_members)
+          end
+
+          #
+          # Resolves a type name.
+          #
+          # @param [Symbol] name
+          # 
+          # @return [Type]
+          #
+          def resolve_symbol(name)
+            @types[name]
+          end
+
+        end
+      end
+    end
+  end
+end
