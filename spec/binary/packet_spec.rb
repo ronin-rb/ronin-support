40d68ad73f2224d149632c8f7c583859f136a664
Postmodern <postmodern.mod3@gmail.com>
2022-05-04 13:28:21 -0700

    Added `Binary::Packet` (closes #227).

---
+++ spec/binary/packet_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+require 'ronin/support/binary/packet'
+
+describe Ronin::Support::Binary::Packet do
+  subject { described_class }
+
+  it "must set .type_system to Ronin::Support::Binary::Types::Network" do
+    expect(subject.type_system).to be(Ronin::Support::Binary::Types::Network)
+  end
+
+  it "must set .alignment to 1" do
+    expect(subject.alignment).to eq(1)
+  end
+
+  it "must set .padding to false" do
+    expect(subject.padding).to be(false)
+  end
+end
