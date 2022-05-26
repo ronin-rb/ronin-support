4a3760bcd5c8cfee7ef9966bb4e51d254586d74d
Postmodern <postmodern.mod3@gmail.com>
2022-05-25 21:10:04 -0700

    Fixed spec titles.

--- spec/compression_spec.rb
+++ spec/compression_spec.rb
@@ -7,13 +7,13 @@ describe Ronin::Support::Compression do
     "x\x9C\xCBH\xCD\xC9\xC9\a\x00\x06,\x02\x15".force_encoding(Encoding::ASCII_8BIT)
   end
 
-  describe "#zlib_inflate" do
+  describe ".zlib_inflate" do
     it "must inflate a zlib deflated String" do
       expect(subject.zlib_inflate(zlib_compressed)).to eq(zlib_uncompressed)
     end
   end
 
-  describe "#zlib_deflate" do
+  describe ".zlib_deflate" do
     it "must zlib deflate a String" do
       expect(subject.zlib_deflate(zlib_uncompressed)).to eq(zlib_compressed)
     end
