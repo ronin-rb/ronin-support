2653503dc58a392721574872d44dc7232d2d6515
Postmodern <postmodern.mod3@gmail.com>
2022-05-25 21:14:55 -0700

    Added `Compression.gzip_stream`, `.gzip_open`, `.gzip`, `.gunzip` (issue #284).

--- lib/ronin/support/compression.rb
+++ lib/ronin/support/compression.rb
@@ -17,12 +17,8 @@
 # along with ronin-support.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-begin
-  require 'zlib'
-rescue ::LoadError
-  warn "WARNING: Ruby was not compiled with zlib support"
-end
-
+require 'ronin/support/compression/zlib'
+require 'ronin/support/compression/gzip'
 require 'ronin/support/compression/core_ext'
 
 module Ronin
@@ -69,6 +65,105 @@ module Ronin
       #
       def self.zlib_deflate(string)
         Zlib::Deflate.deflate(string)
+      end
+
+      #
+      # Creates a gzip stream around the IO object.
+      #
+      # @param [IO, StringIO] io
+      #   The IO object to read or write data to.
+      #
+      # @yield [gz]
+      #   If a block is given, it will be passed the gzip stream object.
+      #
+      # @yieldparam [Zlib::GzipReader, Zlib::GzipWriter] gz
+      #   The gzip reader or writer object.
+      #
+      # @return [Zlib::GzipReader, Zlib::GzipWriter]
+      #   The gzip reader or writer object.
+      #
+      # @raise [ArgumentError]
+      #   The mode must include either `r`, `w`, or `a`.
+      #
+      # @see https://rubydoc.info/stdlib/zlib/Zlib/GzipReader
+      # @see https://rubydoc.info/stdlib/zlib/Zlib/GzipWriter
+      #
+      # @api public
+      #
+      def self.gzip_stream(io, mode: 'r', &block)
+        GZip.wrap(io,mode: mode,&block)
+      end
+
+      #
+      # Creates a gzip output stream around an IO object.
+      #
+      # @param [IO, StringIO] io
+      #   The output IO object to write the gzipped data to.
+      #
+      # @yield [output]
+      #   If a block is given, it will be passed the gzip writer object.
+      #
+      # @yieldparam [Zlib::GzipWriter] output
+      #   The gzip writer object.
+      #
+      # @return [Zlib::GzipWriter]
+      #   The gzip writer object.
+      #
+      # @raise [ArgumentError]
+      #   The mode must include either `r`, `w`, or `a`.
+      #
+      # @see https://rubydoc.info/stdlib/zlib/Zlib/GzipWriter
+      #
+      # @api public
+      #
+      def self.gzip_open(path, mode: 'r', &block)
+        GZip.open(path,mode: mode,&block)
+      end
+
+      #
+      # Opens the gzipped file for reading.
+      #
+      # @param [String] path
+      #   The path to the file to read.
+      #
+      # @yield [gz]
+      #   If a block is given, it will be passed the gzip reader object.
+      #
+      # @yieldparam [Zlib::GzipReader] gz
+      #   The gzip reader object.
+      #
+      # @return [Zlib::GzipReader]
+      #   The gzip reader object.
+      #
+      # @see gzip_open
+      #
+      # @api public
+      #
+      def self.gunzip(path,&block)
+        gzip_open(path,&block)
+      end
+
+      #
+      # Opens the gzip file for writing.
+      #
+      # @param [String] path
+      #   The path to the file to write to.
+      #
+      # @yield [gz]
+      #   If a block is given, it will be passed the gzip writer object.
+      #
+      # @yieldparam [Zlib::GzipWriter] gz
+      #   The gzip writer object.
+      #
+      # @return [Zlib::GzipWriter]
+      #   The gzip writer object.
+      #
+      # @see gzip_open
+      #
+      # @api public
+      #
+      def self.gzip(path,&block)
+        gzip_open(path,mode: 'w',&block)
       end
     end
   end
