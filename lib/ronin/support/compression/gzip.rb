cf7d7524cbdcb7cc4f9296d1a27562fd16b90e51
Postmodern <postmodern.mod3@gmail.com>
2022-05-25 20:43:04 -0700

    Added `Ronin::Support::Compression::GZip` (issue #284).

---
+++ lib/ronin/support/compression/gzip.rb
@@ -0,0 +1,115 @@
+#
+# Copyright (c) 2006-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of ronin-support.
+#
+# ronin-support is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-support is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-support.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/support/compression/zlib'
+
+require 'stringio'
+require 'tempfile'
+
+module Ronin
+  module Support
+    module Compression
+      #
+      # Handles GZip compression.
+      #
+      # @api public
+      #
+      # @since 1.0.0
+      #
+      module GZip
+        #
+        # Creates a gzip stream around the IO object.
+        #
+        # @param [IO, Tempfile, StringIO, String] io
+        #   The IO object to read or write data to.
+        #
+        # @yield [gz]
+        #   If a block is given, it will be passed the gzip stream object.
+        #
+        # @yieldparam [Zlib::GzipReader, Zlib::GzipWriter] gz
+        #   The gzip reader or writer object.
+        #
+        # @return [Zlib::GzipReader, Zlib::GzipWriter]
+        #   The gzip reader or writer object.
+        #
+        # @raise [ArgumentError]
+        #   The IO object must be either an `IO`, `Tempfile`, `StringIO`, or
+        #   `String` object. The mode must include either `r`, `w`, or `a`.
+        #
+        # @see https://rubydoc.info/stdlib/zlib/Zlib/GzipReader
+        # @see https://rubydoc.info/stdlib/zlib/Zlib/GzipWriter
+        #
+        # @api public
+        #
+        def self.wrap(io, mode: 'r', &block)
+          io = case io
+               when String                 then StringIO.new(io)
+               when IO, Tempfile, StringIO then io
+               else
+                 raise(ArgumentError,"IO argument must be either a IO, Tempfile, StringIO, or String")
+               end
+
+          gzip_class = if mode.include?('w') || mode.include?('a')
+                         Zlib::GzipWriter
+                       elsif mode.include?('r')
+                         Zlib::GzipReader
+                       else
+                         raise(ArgumentError,"mode argument must include either 'r', 'w', or 'a': #{mode.inspect}")
+                       end
+
+          return gzip_class.wrap(io,&block)
+        end
+
+        #
+        # Creates a gzip output stream around an IO object.
+        #
+        # @param [IO, StringIO] io
+        #   The output IO object to write the gzipped data to.
+        #
+        # @yield [gz]
+        #   If a block is given, it will be passed the gzip writer object.
+        #
+        # @yieldparam [Zlib::GzipWriter] gz
+        #   The gzip writer object.
+        #
+        # @return [Zlib::GzipWriter]
+        #   The gzip writer object.
+        #
+        # @raise [ArgumentError]
+        #   The mode must include either `r`, `w`, or `a`.
+        #
+        # @see https://rubydoc.info/stdlib/zlib/Zlib/GzipWriter
+        #
+        # @api public
+        #
+        def self.open(path, mode: 'r', &block)
+          gzip_class = if mode.include?('w') || mode.include?('a')
+                         Zlib::GzipWriter
+                       elsif mode.include?('r')
+                         Zlib::GzipReader
+                       else
+                         raise(ArgumentError,"mode argument must include either 'r', 'w', or 'a'")
+                       end
+
+          return gzip_class.open(path,&block)
+        end
+      end
+    end
+  end
+end
---
+++ spec/compression/gzip_spec.rb
@@ -0,0 +1,327 @@
+require 'spec_helper'
+require 'ronin/support/compression/gzip'
+
+require 'tempfile'
+
+describe Ronin::Support::Compression::GZip do
+  let(:fixtures_dir) { File.join(__dir__,'fixtures') }
+
+  let(:txt_path) { File.join(fixtures_dir,'file.txt') }
+  let(:txt_data) { File.read(txt_path)                }
+
+  let(:gzip_path) { File.join(fixtures_dir,'file.gz') }
+  let(:gzip_data) { File.binread(gzip_path)           }
+
+  describe ".wrap" do
+    context "when given a String" do
+      let(:string) { gzip_data }
+
+      it "must return a Zlib::GzipReader object" do
+        expect(subject.wrap(string)).to be_kind_of(Zlib::GzipReader)
+      end
+
+      context "and when a block is given" do
+        it "must yield the new Zlib::GzipReader object" do
+          expect { |b|
+            subject.wrap(string,&b)
+          }.to yield_with_args(Zlib::GzipReader)
+        end
+      end
+
+      it "must wrap the String in a StringIO object" do
+        gz = subject.wrap(string)
+
+        expect(gz.read).to eq(txt_data)
+      end
+
+      context "when given mode: 'w'" do
+        let(:tempfile) { Tempfile.new('ronin-support') }
+        let(:path)     { tempfile.path }
+
+        it "must return a Zlib::GzipWriter object" do
+          expect(subject.wrap(string, mode: 'w')).to be_kind_of(Zlib::GzipWriter)
+        end
+
+        context "and when a block is given" do
+          it "must yield the new Zlib::GzipWriter object" do
+            expect { |b|
+              subject.wrap(string, mode: 'w', &b)
+            }.to yield_with_args(Zlib::GzipWriter)
+          end
+        end
+      end
+
+      context "when given mode: 'a'" do
+        let(:tempfile) { Tempfile.new('ronin-support') }
+        let(:path)     { tempfile.path }
+
+        it "must return a Zlib::GzipWriter object" do
+          expect(subject.wrap(string, mode: 'a')).to be_kind_of(Zlib::GzipWriter)
+        end
+
+        context "and when a block is given" do
+          it "must yield the new Zlib::GzipWriter object" do
+            expect { |b|
+              subject.wrap(string, mode: 'a', &b)
+            }.to yield_with_args(Zlib::GzipWriter)
+          end
+        end
+      end
+
+      context "when given an unknown mode String" do
+        let(:mode) { 'x' }
+
+        it do
+          expect {
+            subject.wrap(string, mode: mode)
+          }.to raise_error(ArgumentError,"mode argument must include either 'r', 'w', or 'a': #{mode.inspect}")
+        end
+      end
+    end
+
+    context "when given a StringIO" do
+      let(:string_io) { StringIO.new(gzip_data) }
+
+      it "must return a Zlib::GzipReader object" do
+        expect(subject.wrap(string_io)).to be_kind_of(Zlib::GzipReader)
+      end
+
+      context "and when a block is given" do
+        it "must yield the new Zlib::GzipReader object" do
+          expect { |b|
+            subject.wrap(string_io,&b)
+          }.to yield_with_args(Zlib::GzipReader)
+        end
+      end
+
+      it "must wrap the String in a StringIO object" do
+        gz = subject.wrap(string_io)
+
+        expect(gz.read).to eq(txt_data)
+      end
+
+      context "when given mode: 'w'" do
+        let(:tempfile) { Tempfile.new('ronin-support') }
+        let(:path)     { tempfile.path }
+
+        it "must return a Zlib::GzipWriter object" do
+          expect(subject.wrap(string_io, mode: 'w')).to be_kind_of(Zlib::GzipWriter)
+        end
+
+        context "and when a block is given" do
+          it "must yield the new Zlib::GzipWriter object" do
+            expect { |b|
+              subject.wrap(string_io, mode: 'w', &b)
+            }.to yield_with_args(Zlib::GzipWriter)
+          end
+        end
+      end
+
+      context "when given mode: 'a'" do
+        let(:tempfile) { Tempfile.new('ronin-support') }
+        let(:path)     { tempfile.path }
+
+        it "must return a Zlib::GzipWriter object" do
+          expect(subject.wrap(string_io, mode: 'a')).to be_kind_of(Zlib::GzipWriter)
+        end
+
+        context "and when a block is given" do
+          it "must yield the new Zlib::GzipWriter object" do
+            expect { |b|
+              subject.wrap(string_io, mode: 'a', &b)
+            }.to yield_with_args(Zlib::GzipWriter)
+          end
+        end
+      end
+
+      context "when given an unknown mode String" do
+        let(:mode) { 'x' }
+
+        it do
+          expect {
+            subject.wrap(string_io, mode: mode)
+          }.to raise_error(ArgumentError,"mode argument must include either 'r', 'w', or 'a': #{mode.inspect}")
+        end
+      end
+    end
+
+    context "when given an IO object" do
+      let(:io) { File.open(gzip_path,'rb') }
+
+      it "must return a Zlib::GzipReader object" do
+        expect(subject.wrap(io)).to be_kind_of(Zlib::GzipReader)
+      end
+
+      context "and when a block is given" do
+        it "must yield the new Zlib::GzipReader object" do
+          expect { |b|
+            subject.wrap(io,&b)
+          }.to yield_with_args(Zlib::GzipReader)
+        end
+      end
+
+      context "when given mode: 'w'" do
+        let(:tempfile) { Tempfile.new('ronin-support') }
+
+        it "must return a Zlib::GzipWriter object" do
+          expect(subject.wrap(tempfile, mode: 'w')).to be_kind_of(Zlib::GzipWriter)
+        end
+
+        context "and when a block is given" do
+          let(:io) { File.open(tempfile.path,'w') }
+
+          it "must yield the new Zlib::GzipWriter object" do
+            expect { |b|
+              subject.wrap(io, mode: 'w', &b)
+            }.to yield_with_args(Zlib::GzipWriter)
+          end
+        end
+      end
+
+      context "when given mode: 'a'" do
+        let(:tempfile) { Tempfile.new('ronin-support') }
+        let(:path)     { tempfile.path }
+
+        it "must return a Zlib::GzipWriter object" do
+          expect(subject.wrap(io, mode: 'a')).to be_kind_of(Zlib::GzipWriter)
+        end
+
+        context "and when a block is given" do
+          let(:io) { File.open(tempfile.path,'w') }
+
+          it "must yield the new Zlib::GzipWriter object" do
+            expect { |b|
+              subject.wrap(io, mode: 'a', &b)
+            }.to yield_with_args(Zlib::GzipWriter)
+          end
+        end
+      end
+
+      context "when given an unknown mode String" do
+        let(:mode) { 'x' }
+
+        it do
+          expect {
+            subject.wrap(io, mode: mode)
+          }.to raise_error(ArgumentError,"mode argument must include either 'r', 'w', or 'a': #{mode.inspect}")
+        end
+      end
+    end
+
+    context "when given a Tempfile" do
+      let(:tempfile) { Tempfile.new('ronin-support') }
+
+      before do
+        tempfile.write(gzip_data)
+        tempfile.flush
+        tempfile.rewind
+      end
+
+      it "must return a Zlib::GzipReader object" do
+        expect(subject.wrap(tempfile)).to be_kind_of(Zlib::GzipReader)
+      end
+
+      context "and when a block is given" do
+        it "must yield the new Zlib::GzipReader object" do
+          expect { |b|
+            subject.wrap(tempfile,&b)
+          }.to yield_with_args(Zlib::GzipReader)
+        end
+      end
+
+      it "must wrap the String in a Tempfile object" do
+        gz = subject.wrap(tempfile)
+
+        expect(gz.read).to eq(txt_data)
+      end
+
+      context "when given mode: 'w'" do
+        it "must return a Zlib::GzipWriter object" do
+          expect(subject.wrap(tempfile, mode: 'w')).to be_kind_of(Zlib::GzipWriter)
+        end
+
+        context "and when a block is given" do
+          it "must yield the new Zlib::GzipWriter object" do
+            expect { |b|
+              subject.wrap(tempfile, mode: 'w', &b)
+            }.to yield_with_args(Zlib::GzipWriter)
+          end
+        end
+      end
+
+      context "when given mode: 'a'" do
+        it "must return a Zlib::GzipWriter object" do
+          expect(subject.wrap(tempfile, mode: 'a')).to be_kind_of(Zlib::GzipWriter)
+        end
+
+        context "and when a block is given" do
+          it "must yield the new Zlib::GzipWriter object" do
+            expect { |b|
+              subject.wrap(tempfile, mode: 'a', &b)
+            }.to yield_with_args(Zlib::GzipWriter)
+          end
+        end
+      end
+
+      context "when given an unknown mode String" do
+        let(:mode) { 'x' }
+
+        it do
+          expect {
+            subject.wrap(tempfile, mode: mode)
+          }.to raise_error(ArgumentError,"mode argument must include either 'r', 'w', or 'a': #{mode.inspect}")
+        end
+      end
+    end
+  end
+
+  describe ".open" do
+    let(:path) { gzip_path }
+
+    it "must return a Zlib::GzipReader object" do
+      expect(subject.open(path)).to be_kind_of(Zlib::GzipReader)
+    end
+
+    context "and when a block is given" do
+      it "must yield the new Zlib::GzipReader object" do
+        expect { |b|
+          subject.open(path,&b)
+        }.to yield_with_args(Zlib::GzipReader)
+      end
+    end
+
+    context "when given mode: 'w'" do
+      let(:tempfile) { Tempfile.new('ronin-support') }
+      let(:path)     { tempfile.path }
+
+      it "must return a Zlib::GzipWriter object" do
+        expect(subject.open(path, mode: 'w')).to be_kind_of(Zlib::GzipWriter)
+      end
+
+      context "and when a block is given" do
+        it "must yield the new Zlib::GzipWriter object" do
+          expect { |b|
+            subject.open(path, mode: 'w', &b)
+          }.to yield_with_args(Zlib::GzipWriter)
+        end
+      end
+    end
+
+    context "when given mode: 'a'" do
+      let(:tempfile) { Tempfile.new('ronin-support') }
+      let(:path)     { tempfile.path }
+
+      it "must return a Zlib::GzipWriter object" do
+        expect(subject.open(path, mode: 'w')).to be_kind_of(Zlib::GzipWriter)
+      end
+
+      context "and when a block is given" do
+        it "must yield the new Zlib::GzipWriter object" do
+          expect { |b|
+            subject.open(path, mode: 'a', &b)
+          }.to yield_with_args(Zlib::GzipWriter)
+        end
+      end
+    end
+  end
+end
