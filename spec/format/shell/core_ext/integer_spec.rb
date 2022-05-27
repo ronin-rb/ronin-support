173443fd3a20d729585a28d37250f3918ec5ec6a
Postmodern <postmodern.mod3@gmail.com>
2022-05-26 17:41:17 -0700

    Added missing edge case specs.

--- spec/format/shell/core_ext/integer_spec.rb
+++ spec/format/shell/core_ext/integer_spec.rb
@@ -6,9 +6,11 @@ describe Integer do
 
   it { expect(subject).to respond_to(:shell_encode) }
   it { expect(subject).to respond_to(:sh_encode) }
+  it { expect(subject).to respond_to(:bash_encode) }
 
   it { expect(subject).to respond_to(:shell_escape) }
   it { expect(subject).to respond_to(:sh_escape) }
+  it { expect(subject).to respond_to(:bash_escape) }
 
   describe "#shell_escape" do
     described_class::SHELL_ESCAPE_BYTES.each do |byte,escaped_char|
@@ -37,7 +39,7 @@ describe Integer do
       end
     end
 
-    context "when called on an Integer between 0x100 and 0xffff" do
+    context "when called on an Integer is greater than 0xff" do
       subject { 0xFFFF }
 
       it "must return the lowercase \"$'\\uXXXX'\" hex escaped String" do
@@ -63,8 +65,20 @@ describe Integer do
   end
 
   describe "#shell_encode" do
-    it "must encode the Integer as a shell hex character" do
-      expect(subject.shell_encode).to eq("$'\\x41'")
+    context "when called on an Integer between 0x00 and 0xff" do
+      subject { 0x41 }
+
+      it "must encode the Integer as a shell hex character" do
+        expect(subject.shell_encode).to eq("$'\\x41'")
+      end
+    end
+
+    context "when called on an Integer is greater than 0xff" do
+      subject { 0xFFFF }
+
+      it "must return the lowercase \"$'\\uXXXX'\" hex escaped String" do
+        expect(subject.shell_escape).to eq("$'\\uffff'")
+      end
     end
   end
 end
