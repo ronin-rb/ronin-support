a51caeb466cda5dab947d46fda0297da21a724ba
Postmodern <postmodern.mod3@gmail.com>
2022-05-30 18:52:43 -0700

    Added top-level `.tar_*` methods to `Compression` (issue #284).

--- spec/compression_spec.rb
+++ spec/compression_spec.rb
@@ -10,6 +10,9 @@ describe Ronin::Support::Compression do
   let(:gzip_path) { File.join(fixtures_dir,'file.gz') }
   let(:gzip_data) { File.binread(gzip_path)           }
 
+  let(:tar_path) { File.join(fixtures_dir,'file.tar') }
+  let(:tar_data) { File.binread(tar_path)             }
+
   let(:zlib_uncompressed) { "hello" }
   let(:zlib_compressed) do
     "x\x9C\xCBH\xCD\xC9\xC9\a\x00\x06,\x02\x15".force_encoding(Encoding::ASCII_8BIT)
@@ -290,6 +293,275 @@ describe Ronin::Support::Compression do
         expect { |b|
           subject.gzip(path,&b)
         }.to yield_with_args(Zlib::GzipWriter)
+      end
+    end
+  end
+
+  describe ".tar_stream" do
+    context "when given a String" do
+      let(:string) { tar_data }
+
+      it "must return a described_class::Tar::Reader object" do
+        expect(subject.tar_stream(string)).to be_kind_of(described_class::Tar::Reader)
+      end
+
+      context "and when a block is given" do
+        it "must yield the new described_class::Tar::Reader object" do
+          expect { |b|
+            subject.tar_stream(string,&b)
+          }.to yield_with_args(described_class::Tar::Reader)
+        end
+      end
+
+      it "must tar_stream the String in a StringIO object" do
+        tar   = subject.tar_stream(string)
+        entry = tar.find { |entry| entry.full_name == 'file.txt' }
+
+        expect(entry.read).to eq(txt_data)
+      end
+
+      context "when given mode: 'w'" do
+        it "must return a described_class::Tar::Writer object" do
+          expect(subject.tar_stream(string, mode: 'w')).to be_kind_of(described_class::Tar::Writer)
+        end
+
+        context "and when a block is given" do
+          it "must yield the new described_class::Tar::Writer object" do
+            expect { |b|
+              subject.tar_stream(string, mode: 'w', &b)
+            }.to yield_with_args(described_class::Tar::Writer)
+          end
+        end
+      end
+
+      context "when given mode: 'a'" do
+        it "must return a described_class::Tar::Writer object" do
+          expect(subject.tar_stream(string, mode: 'a')).to be_kind_of(described_class::Tar::Writer)
+        end
+
+        context "and when a block is given" do
+          it "must yield the new described_class::Tar::Writer object" do
+            expect { |b|
+              subject.tar_stream(string, mode: 'a', &b)
+            }.to yield_with_args(described_class::Tar::Writer)
+          end
+        end
+      end
+
+      context "when given an unknown mode String" do
+        let(:mode) { 'x' }
+
+        it do
+          expect {
+            subject.tar_stream(string, mode: mode)
+          }.to raise_error(ArgumentError,"mode argument must include either 'r', 'w', or 'a': #{mode.inspect}")
+        end
+      end
+    end
+
+    context "when given a StringIO" do
+      let(:string_io) { StringIO.new(tar_data) }
+
+      it "must return a described_class::Tar::Reader object" do
+        expect(subject.tar_stream(string_io)).to be_kind_of(described_class::Tar::Reader)
+      end
+
+      context "and when a block is given" do
+        it "must yield the new described_class::Tar::Reader object" do
+          expect { |b|
+            subject.tar_stream(string_io,&b)
+          }.to yield_with_args(described_class::Tar::Reader)
+        end
+      end
+
+      it "must tar_stream the String in a StringIO object" do
+        tar   = subject.tar_stream(string_io)
+        entry = tar.find { |entry| entry.full_name == 'file.txt' }
+
+        expect(entry.read).to eq(txt_data)
+      end
+
+      context "when given mode: 'w'" do
+        it "must return a described_class::Tar::Writer object" do
+          expect(subject.tar_stream(string_io, mode: 'w')).to be_kind_of(described_class::Tar::Writer)
+        end
+
+        context "and when a block is given" do
+          it "must yield the new described_class::Tar::Writer object" do
+            expect { |b|
+              subject.tar_stream(string_io, mode: 'w', &b)
+            }.to yield_with_args(described_class::Tar::Writer)
+          end
+        end
+      end
+
+      context "when given mode: 'a'" do
+        it "must return a described_class::Tar::Writer object" do
+          expect(subject.tar_stream(string_io, mode: 'a')).to be_kind_of(described_class::Tar::Writer)
+        end
+
+        context "and when a block is given" do
+          it "must yield the new described_class::Tar::Writer object" do
+            expect { |b|
+              subject.tar_stream(string_io, mode: 'a', &b)
+            }.to yield_with_args(described_class::Tar::Writer)
+          end
+        end
+      end
+
+      context "when given an unknown mode String" do
+        let(:mode) { 'x' }
+
+        it do
+          expect {
+            subject.tar_stream(string_io, mode: mode)
+          }.to raise_error(ArgumentError,"mode argument must include either 'r', 'w', or 'a': #{mode.inspect}")
+        end
+      end
+    end
+
+    context "when given an IO object" do
+      let(:io) { File.open(tar_path,'rb') }
+
+      it "must return a described_class::Tar::Reader object" do
+        expect(subject.tar_stream(io)).to be_kind_of(described_class::Tar::Reader)
+      end
+
+      context "and when a block is given" do
+        it "must yield the new described_class::Tar::Reader object" do
+          expect { |b|
+            subject.tar_stream(io,&b)
+          }.to yield_with_args(described_class::Tar::Reader)
+        end
+      end
+
+      context "when given mode: 'w'" do
+        let(:tempfile) { Tempfile.new('ronin-support') }
+        let(:path)     { tempfile.path }
+        let(:io)       { File.open(path,'wb') }
+
+        it "must return a described_class::Tar::Writer object" do
+          expect(subject.tar_stream(tempfile, mode: 'w')).to be_kind_of(described_class::Tar::Writer)
+        end
+
+        context "and when a block is given" do
+          it "must yield the new described_class::Tar::Writer object" do
+            expect { |b|
+              subject.tar_stream(io, mode: 'w', &b)
+            }.to yield_with_args(described_class::Tar::Writer)
+          end
+        end
+      end
+
+      context "when given mode: 'a'" do
+        let(:tempfile) { Tempfile.new('ronin-support') }
+        let(:path)     { tempfile.path }
+        let(:io)       { File.open(path,'ab') }
+
+        it "must return a described_class::Tar::Writer object" do
+          expect(subject.tar_stream(io, mode: 'a')).to be_kind_of(described_class::Tar::Writer)
+        end
+
+        context "and when a block is given" do
+          it "must yield the new described_class::Tar::Writer object" do
+            expect { |b|
+              subject.tar_stream(io, mode: 'a', &b)
+            }.to yield_with_args(described_class::Tar::Writer)
+          end
+        end
+      end
+
+      context "when given an unknown mode String" do
+        let(:mode) { 'x' }
+
+        it do
+          expect {
+            subject.tar_stream(io, mode: mode)
+          }.to raise_error(ArgumentError,"mode argument must include either 'r', 'w', or 'a': #{mode.inspect}")
+        end
+      end
+    end
+  end
+
+  describe ".tar_open" do
+    let(:path) { tar_path }
+
+    it "must return a described_class::Tar::Reader object" do
+      expect(subject.tar_open(path)).to be_kind_of(described_class::Tar::Reader)
+    end
+
+    context "and when a block is given" do
+      it "must yield the new described_class::Tar::Reader object" do
+        expect { |b|
+          subject.tar_open(path,&b)
+        }.to yield_with_args(described_class::Tar::Reader)
+      end
+    end
+
+    context "when given mode: 'w'" do
+      let(:tempfile) { Tempfile.new('ronin-support') }
+      let(:path)     { tempfile.path }
+
+      it "must return a described_class::Tar::Writer object" do
+        expect(subject.tar_open(path, mode: 'w')).to be_kind_of(described_class::Tar::Writer)
+      end
+
+      context "and when a block is given" do
+        it "must yield the new described_class::Tar::Writer object" do
+          expect { |b|
+            subject.tar_open(path, mode: 'w', &b)
+          }.to yield_with_args(described_class::Tar::Writer)
+        end
+      end
+    end
+
+    context "when given mode: 'a'" do
+      let(:tempfile) { Tempfile.new('ronin-support') }
+      let(:path)     { tempfile.path }
+
+      it "must return a described_class::Tar::Writer object" do
+        expect(subject.tar_open(path, mode: 'w')).to be_kind_of(described_class::Tar::Writer)
+      end
+
+      context "and when a block is given" do
+        it "must yield the new described_class::Tar::Writer object" do
+          expect { |b|
+            subject.tar_open(path, mode: 'a', &b)
+          }.to yield_with_args(described_class::Tar::Writer)
+        end
+      end
+    end
+  end
+
+  describe ".untar" do
+    let(:path) { tar_path }
+
+    it "must return a described_class::Tar::Reader object" do
+      expect(subject.untar(path)).to be_kind_of(described_class::Tar::Reader)
+    end
+
+    context "and when a block is given" do
+      it "must yield the new described_class::Tar::Reader object" do
+        expect { |b|
+          subject.untar(path,&b)
+        }.to yield_with_args(described_class::Tar::Reader)
+      end
+    end
+  end
+
+  describe ".tar" do
+    let(:tempfile) { Tempfile.new('ronin-support') }
+    let(:path)     { tempfile.path }
+
+    it "must return a described_class::Tar::Writer object" do
+      expect(subject.tar(path)).to be_kind_of(described_class::Tar::Writer)
+    end
+
+    context "and when a block is given" do
+      it "must yield the new described_class::Tar::Writer object" do
+        expect { |b|
+          subject.tar(path,&b)
+        }.to yield_with_args(described_class::Tar::Writer)
       end
     end
   end
