b34afc62d8d5a97d7e26bf355f97e859e28d3151
Postmodern <postmodern.mod3@gmail.com>
2022-06-08 22:18:06 -0700

    Added `Network::ASN::List` (closes #310).

---
+++ lib/ronin/support/network/asn/list.rb
@@ -0,0 +1,260 @@
+#
+# Copyright (c) 2006-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of ronin-support.
+#
+# ronin-support is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-support is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-support.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/support/network/asn/record'
+require 'ronin/support/network/asn/record_set'
+require 'ronin/support/network/ip_range/range'
+require 'ronin/support/home'
+
+require 'net/https'
+require 'zlib'
+
+module Ronin
+  module Support
+    module Network
+      module ASN
+        #
+        # Represents the entire list of all IPv4 and IPv6 ASNs.
+        #
+        # @api public
+        #
+        # @since 1.0.0
+        #
+        class List < RecordSet
+
+          include Enumerable
+
+          # File name of the combined IPv4 and IPv6 ASN list file.
+          FILE_NAME = 'ip2asn-combined.tsv.gz'
+
+          # The `https://iptoasn.com/data/ip2asn-combined.tsv.gz` URL.
+          URL = "https://iptoasn.com/data/#{FILE_NAME}"
+
+          # The path to `~/.local/share/ronin/ronin-support/ip2asn-combined.tsv.gz` list file.
+          PATH = File.join(Home::LOCAL_SHARE_DIR,'ronin','ronin-support',FILE_NAME)
+
+          # The path to the list file.
+          #
+          # @return [String]
+          attr_reader :path
+
+          #
+          # Initializes the list file.
+          #
+          # @param [String] path
+          #   The path to the list file.
+          #
+          # @api private
+          #
+          def initialize(path)
+            super()
+
+            @path = path
+
+            @ipv4_prefixes = {}
+            @ipv6_prefixes = {}
+          end
+
+          #
+          # Determines whether the list file has been previously downloaded.
+          #
+          # @param [String] path
+          #   An optional alternate path to the list file.
+          #
+          # @return [Boolean]
+          #
+          def self.downloaded?(path=PATH)
+            File.file?(path)
+          end
+
+          # One day in seconds.
+          ONE_DAY = 60 * 60 * 24
+
+          #
+          # Determines if the downloaded list file is older than one day.
+          #
+          # @param [String] path
+          #   An optional alternate path to the list file.
+          #
+          # @return [Boolean]
+          #
+          def self.stale?(path=PATH)
+            !File.file?(path) || File.stat(path).mtime < (Time.now - ONE_DAY)
+          end
+
+          #
+          # Downloads the list file.
+          #
+          # @param [String] url
+          #   An optional alternate URL to download the `ip2asn-combined.tsv.gz`
+          #   file.
+          #
+          # @param [String] path
+          #   An optional alternate path to the list file.
+          #
+          def self.download(url: URL, path: PATH)
+            uri = URI(url)
+
+            Net::HTTP.start(uri.host,uri.port, use_ssl: true) do |http|
+              request  = Net::HTTP::Get.new(uri.path)
+
+              http.request(request) do |response|
+                File.open("#{path}.part",'wb') do |file|
+                  response.read_body do |chunk|
+                    file.write(chunk)
+                  end
+                end
+
+                FileUtils.mv("#{path}.part",path)
+              end
+            end
+          end
+
+          #
+          # Optionally update the cached list file if it is older than one day.
+          #
+          # @param [String] url
+          #   An optional alternate URL to download the `ip2asn-combined.tsv.gz`
+          #   file.
+          #
+          # @param [String] path
+          #   An optional alternate path to the list file.
+          #
+          def self.update(url: URL, path: PATH)
+            download(url: url, path: path) if stale?(path)
+          end
+
+          #
+          # Parses the contents of the list file.
+          #
+          # @param [String] path
+          #   An optional alternate path to the list file.
+          #
+          # @yield [record]
+          #   If a block is given, it will be passed each parsed record from the
+          #   list file.
+          #
+          # @yieldparam [Record] record
+          #   A parsed record in the list file.
+          #
+          # @return [Enumerator]
+          #   If no block is given, an Enumerator object will be returned.
+          #
+          def self.parse(path=PATH)
+            return enum_for(__method__,path) unless block_given?
+
+            io = if File.extname(path) == '.gz' then Zlib::GzipReader.open(path)
+                 else                                File.new(path)
+                 end
+
+            io.each_line do |line|
+              line.chomp!
+
+              first, last, number, country_code, name = line.split("\t",5)
+              range  = IPRange::Range.new(first,last)
+              number = number.to_i
+              country_code = nil if country_code == 'None'
+              name         = nil if name         == 'Not routed'
+
+              yield Record.new(number,range,country_code,name)
+            end
+          end
+
+          #
+          # Loads the contents of the list file into memory.
+          #
+          # @param [String] path
+          #   An optional alternate path to the list file.
+          #
+          # @return [List]
+          #   The loaded list.
+          #
+          def self.load_file(path=PATH)
+            list = new(path)
+
+            parse(path) do |record|
+              list << record
+            end
+
+            return list
+          end
+
+          #
+          # Adds a record to the list.
+          #
+          # @param [Record] record
+          #
+          # @return [self]
+          #
+          # @api private
+          #
+          def <<(record)
+            super(record)
+
+            prefixes = if record.range.ipv6? then @ipv6_prefixes
+                       else                       @ipv4_prefixes
+                       end
+            (prefixes[record.range.prefix] ||= []) << record
+            return self
+          end
+
+          #
+          # Finds the ASN record for the given IP address.
+          #
+          # @param [IP, IPaddr, String] ip
+          #   The IP address to search for.
+          #
+          # @return [Record, nil]
+          #   The ASN record for the IP address or `nil` if none could be found.
+          #
+          def ip(ip)
+            address = ip.to_s
+            ip      = IPAddr.new(ip) unless ip.kind_of?(IPAddr)
+
+            prefixes = if ip.ipv6? then @ipv6_prefixes
+                       else             @ipv4_prefixes
+                       end
+
+            prefixes.each do |prefix,records|
+              if address.start_with?(prefix)
+                records.each do |record|
+                  if record.include?(ip)
+                    return record
+                  end
+                end
+              end
+            end
+
+            return nil
+          end
+
+          #
+          # Inspects the list.
+          #
+          # @return [String]
+          #
+          def inspect
+            "#<#{self.class}: #{@path}>"
+          end
+
+        end
+      end
+    end
+  end
+end
---
+++ spec/network/asn/list_spec.rb
@@ -0,0 +1,374 @@
+require 'spec_helper'
+require 'ronin/support/network/asn/list'
+
+require 'fileutils'
+require 'tmpdir'
+
+describe Ronin::Support::Network::ASN::List do
+  let(:fixtures_dir) { File.join(__dir__,'fixtures')      }
+  let(:list_file)    { File.join(fixtures_dir,'list.tsv') }
+  let(:records) do
+    Ronin::Support::Network::ASN::List.parse(list_file).to_a
+  end
+
+  let(:path) { list_file }
+  subject { described_class.new(path) }
+
+  it { expect(described_class).to be < Ronin::Support::Network::ASN::RecordSet }
+
+  describe "FILE_NAME" do
+    subject { described_class::FILE_NAME }
+
+    it do
+      expect(subject).to eq('ip2asn-combined.tsv.gz')
+    end
+  end
+
+  describe "URL" do
+    subject { described_class::URL }
+
+    it do
+      expect(subject).to eq('https://iptoasn.com/data/ip2asn-combined.tsv.gz')
+    end
+  end
+
+  describe "PATH" do
+    subject { described_class::PATH }
+
+    it do
+      expect(subject).to eq(File.join(Gem.user_home,'.local','share','ronin','ronin-support','ip2asn-combined.tsv.gz'))
+    end
+  end
+
+  describe "#initialize" do
+    it "must set #path" do
+      expect(subject.path).to eq(path)
+    end
+
+    it "must initializes #records to []" do
+      expect(subject.records).to eq([])
+    end
+  end
+
+  describe "file methods" do
+    let(:tempdir) { Dir.mktmpdir('ronin-support') }
+    let(:path)    { File.join(tempdir,described_class::FILE_NAME) }
+
+    before do
+      stub_const("#{described_class}::PATH",path)
+    end
+
+    describe ".downloaded?" do
+      subject { described_class }
+
+      context "when given no argument" do
+        context "when the file does not exist" do
+          it "must return false" do
+            expect(subject.downloaded?).to be(false)
+          end
+        end
+
+        context "when the file does exist" do
+          before { FileUtils.touch(path) }
+
+          it "must return true" do
+            expect(subject.downloaded?).to be(true)
+          end
+
+          after { FileUtils.rm(path) }
+        end
+      end
+
+      context "when given a path" do
+        context "when the file does not exist" do
+          it "must return false" do
+            expect(subject.downloaded?(path)).to be(false)
+          end
+        end
+
+        context "when the file does exist" do
+          before { FileUtils.touch(path) }
+
+          it "must return true" do
+            expect(subject.downloaded?(path)).to be(true)
+          end
+
+          after { FileUtils.rm(path) }
+        end
+      end
+    end
+
+    describe ".stale?" do
+      subject { described_class }
+
+      context "when given no argument" do
+        context "when the file does not exist" do
+          it "must return true" do
+            expect(subject.stale?).to be(true)
+          end
+        end
+
+        context "when the file does exist" do
+          before { FileUtils.touch(path) }
+
+          context "and the modification time of the file is within the last day" do
+            before do
+              FileUtils.touch(path, mtime: (Time.now - (60 * 60)))
+            end
+
+            it "must return false" do
+              expect(subject.stale?).to be(false)
+            end
+          end
+
+          context "but the modification time is older than one day" do
+            before do
+              FileUtils.touch(path, mtime: (Time.now - (60 * 60 * 25)))
+            end
+
+            it "must return true" do
+              expect(subject.stale?).to be(true)
+            end
+          end
+
+          after { FileUtils.rm(path) }
+        end
+      end
+
+      context "when given a path" do
+        context "when the file does not exist" do
+          it "must return true" do
+            expect(subject.stale?(path)).to be(true)
+          end
+        end
+
+        context "when the file does exist" do
+          before { FileUtils.touch(path) }
+
+          context "and the modification time of the file is within the last day" do
+            before do
+              FileUtils.touch(path, mtime: (Time.now - (60 * 60)))
+            end
+
+            it "must return false" do
+              expect(subject.stale?(path)).to be(false)
+            end
+          end
+
+          context "but the modification time is older than one day" do
+            before do
+              FileUtils.touch(path, mtime: (Time.now - (60 * 60 * 25)))
+            end
+
+            it "must return true" do
+              expect(subject.stale?(path)).to be(true)
+            end
+          end
+
+          after { FileUtils.rm(path) }
+        end
+      end
+    end
+
+    describe ".download", :network do
+      subject { described_class }
+
+      context "when given no keyword arguments" do
+        before { subject.download }
+
+        it "must download the URL to the PATH" do
+          expect(File.file?(described_class::PATH)).to be(true)
+          expect(File.stat(described_class::PATH).size).to be > 0
+        end
+      end
+    end
+
+    describe ".update" do
+      subject { described_class }
+
+      context "when given no keyword arguments" do
+        context "and when .stale? returns true" do
+          before { allow(subject).to receive(:stale?).and_return(true) }
+
+          it "must call .download" do
+            expect(subject).to receive(:download)
+
+            subject.update
+          end
+        end
+
+        context "and when .stale? returns false" do
+          before { allow(subject).to receive(:stale?).and_return(false) }
+
+          it "must not call .download" do
+            expect(subject).to_not receive(:download)
+
+            subject.update
+          end
+        end
+      end
+    end
+
+    describe ".parse" do
+      let(:path) { list_file }
+
+      subject { described_class }
+
+      context "when given no argument" do
+        let(:path) { File.join(fixtures_dir,'list.tsv.gz') }
+
+        context "and when given a block" do
+          it "must yield each parsed Ronin::Support::Network::ASN::Record" do
+            expect { |b|
+              subject.parse(&b)
+            }.to yield_successive_args(*records)
+          end
+        end
+
+        context "and when given no block" do
+          it "must return an Enumerator for .parse" do
+            expect(subject.parse.to_a).to eq(records)
+          end
+        end
+      end
+
+      context "when given a path" do
+        context "and when given a block" do
+          it "must yield each parsed Ronin::Support::Network::ASN::Record" do
+            expect { |b|
+              subject.parse(path,&b)
+            }.to yield_successive_args(*records)
+          end
+
+          context "but when the path ends in '.gz'" do
+            let(:path) { File.join(fixtures_dir,'list.tsv.gz') }
+
+            it "must uncompress the gzip data" do
+              expect { |b|
+                subject.parse(&b)
+              }.to yield_successive_args(*records)
+            end
+          end
+        end
+
+        context "and when given no block" do
+          it "must return an Enumerator for .parse" do
+            expect(subject.parse(path).to_a).to eq(records)
+          end
+
+          context "but when the path ends in '.gz'" do
+            let(:path) { File.join(fixtures_dir,'list.tsv.gz') }
+
+            it "must uncompress the gzip data" do
+              expect(subject.parse.to_a).to eq(records)
+            end
+          end
+        end
+      end
+
+      describe "record" do
+        subject { super().parse.first }
+
+        it "must parse the first and second IPs into a Ronin::Support::Network::IPRange::Range" do
+          expect(subject.range).to be_kind_of(Ronin::Support::Network::IPRange::Range)
+          expect(subject.range.begin).to eq(Ronin::Support::Network::IP.new('1.0.0.0'))
+          expect(subject.range.end).to eq(Ronin::Support::Network::IP.new('1.0.0.255'))
+        end
+
+        it "must parse the AS number into an Integer" do
+          expect(subject.number).to eq(13335)
+        end
+
+        it "must parse the country code" do
+          expect(subject.country_code).to eq('US')
+        end
+
+        it "must parse the name" do
+          expect(subject.name).to eq('CLOUDFLARENET')
+        end
+      end
+    end
+
+    describe ".load_file" do
+      let(:path) { list_file }
+
+      subject { described_class }
+
+      context "when given no argument" do
+        let(:path) { File.join(fixtures_dir,'list.tsv.gz') }
+
+        subject { super().load_file }
+
+        it "must return a #{described_class} object with the parsed records" do
+          expect(subject).to be_kind_of(described_class)
+          expect(subject.records).to eq(records)
+        end
+      end
+
+      context "when given a path" do
+        subject { super().load_file(path) }
+
+        it "must return a #{described_class} object with the parsed records" do
+          expect(subject).to be_kind_of(described_class)
+          expect(subject.records).to eq(records)
+        end
+
+        context "but when the path ends in '.gz'" do
+          let(:path) { File.join(fixtures_dir,'list.tsv.gz') }
+
+          it "must uncompress the gzip data" do
+            expect(subject).to be_kind_of(described_class)
+            expect(subject.records).to eq(records)
+          end
+        end
+      end
+    end
+  end
+
+  describe "#<<" do
+    let(:record1) { records[0] }
+    let(:record2) { records[1] }
+
+    it "must append the record to #records" do
+      subject << record1
+      subject << record2
+
+      expect(subject.records).to eq([record1, record2])
+    end
+
+    it "must return self" do
+      expect(subject << record1).to be(subject)
+    end
+  end
+
+  describe "#ip" do
+    subject { described_class.load_file(path) }
+
+    context "when one of the record's range includes the IP address" do
+      let(:ip) { '1.1.1.1' }
+
+      subject { super().ip(ip) }
+
+      it "must return the record who's range includes the IP address" do
+        expect(subject).to be_kind_of(Ronin::Support::Network::ASN::Record)
+        expect(subject.range).to include(ip)
+      end
+    end
+
+    context "when no record's range includes the IP address" do
+      let(:ip) { '255.255.255.255' }
+
+      subject { super().ip(ip) }
+
+      it "must return nil" do
+        expect(subject).to be(nil)
+      end
+    end
+  end
+
+  describe "#inspect" do
+    it "must include the class name and the #path" do
+      expect(subject.inspect).to eq("#<#{described_class}: #{path}>")
+    end
+  end
+end
