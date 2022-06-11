52ad1260fcb3523ad37abddd9b2628ba25639b19
Postmodern <postmodern.mod3@gmail.com>
2022-06-10 19:33:23 -0700

    Moved Net::HTTPRequest building logic into `Network::HTTP::Request` (issue #87).

---
+++ lib/ronin/support/network/http/request.rb
@@ -0,0 +1,175 @@
+#
+# Copyright (c) 2006-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of ronin-support.
+#
+# ronin-support is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-support is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-support.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'net/http'
+require 'uri/query_params'
+
+module Ronin
+  module Support
+    module Network
+      class HTTP
+        #
+        # Handles building HTTP request objects.
+        #
+        # @api private
+        #
+        # @since 1.0.0
+        #
+        module Request
+          #
+          # Builds the [Request-URI], aka path + query string, for a HTTP
+          # request.
+          #
+          # [Request-URI]: https://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html#sec5.1.2
+          #
+          # @param [String] path
+          #
+          # @param [String, nil] query
+          #
+          # @param [Hash{String,Symbol => Object}, nil] query_params
+          #
+          # @return [String]
+          #
+          def self.request_uri(path, query: nil, query_params: nil)
+            if query_params
+              query = URI::QueryParams.dump(query_params)
+            end
+
+            if query
+              # append the query-string onto the path
+              path += if path.include?('?') then "&#{query}"
+                      else                       "?#{query}"
+                      end
+            end
+
+            return path
+          end
+
+          # Request methods and `Net::HTTP` request classes.
+          METHODS = {
+            copy:      Net::HTTP::Copy,
+            delete:    Net::HTTP::Delete,
+            get:       Net::HTTP::Get,
+            head:      Net::HTTP::Head,
+            lock:      Net::HTTP::Lock,
+            mkcol:     Net::HTTP::Mkcol,
+            move:      Net::HTTP::Move,
+            options:   Net::HTTP::Options,
+            patch:     Net::HTTP::Patch,
+            post:      Net::HTTP::Post,
+            propfind:  Net::HTTP::Propfind,
+            proppatch: Net::HTTP::Proppatch,
+            put:       Net::HTTP::Put,
+            trace:     Net::HTTP::Trace,
+            unlock:    Net::HTTP::Unlock
+          }
+
+          #
+          # Creates a new `Net::HTTP` request.
+          #
+          # @param [Symbol] method
+          #
+          # @param [String] path
+          #
+          # @param [String, nil] query
+          #   The query-string to append to the request path.
+          #
+          # @param [Hash, nil] query_params
+          #   The query-params to append to the request path.
+          #
+          # @param [String, nil] body
+          #   The body of the request.
+          #
+          # @param [Hash, String, nil] form_data
+          #   The form data that may be sent in the body of the request.
+          #
+          # @param [String, nil] user
+          #   The user to authenticate as.
+          #
+          # @param [String, nil] password
+          #   The password to authenticate with.
+          #
+          # @param [Hash{Symbol,String => String}, nil] headers
+          #   Additional HTTP header names and values to add to the request.
+          #
+          # @return [Net::HTTP::Copy,
+          #          Net::HTTP::Delete,
+          #          Net::HTTP::Get,
+          #          Net::HTTP::Head,
+          #          Net::HTTP::Lock,
+          #          Net::HTTP::Mkcol,
+          #          Net::HTTP::Move,
+          #          Net::HTTP::Options,
+          #          Net::HTTP::Patch,
+          #          Net::HTTP::Post,
+          #          Net::HTTP::Propfind,
+          #          Net::HTTP::Proppatch,
+          #          Net::HTTP::Put,
+          #          Net::HTTP::Trace,
+          #          Net::HTTP::Unlock]
+          #   The built HTTP request object.
+          #
+          def self.build(method,path, headers: nil,
+                                      # Basic-Auth keyword arguments
+                                      user:     nil,
+                                      password: nil,
+                                      # query string keyword arguments
+                                      query:        nil,
+                                      query_params: nil,
+                                      # request body keyword arguments
+                                      body:      nil,
+                                      form_data: nil)
+            request_class = METHODS.fetch(method) do
+              raise(ArgumentError,"unknown HTTP request method: #{method.inspect}")
+            end
+
+            request = request_class.new(
+              request_uri(path, query: query, query_params: query_params),
+              headers
+            )
+
+            if user
+              user     = user.to_s
+              password = password.to_s if password
+
+              request.basic_auth(user,password)
+            end
+
+            if form_data
+              case form_data
+              when String
+                request.content_type = 'application/x-www-form-urlencoded'
+                request.body         = form_data
+              else
+                request.form_data    = form_data
+              end
+            elsif body
+              case body
+              when IO, StringIO then request.body_stream = body
+              else                   request.body        = body
+              end
+            end
+
+            return request
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/network/http/request_spec.rb
@@ -0,0 +1,440 @@
+require 'spec_helper'
+require 'ronin/support/network/http/request'
+
+describe Ronin::Support::Network::HTTP::Request do
+  describe ".request_uri" do
+    let(:path) { '/foo' }
+
+    context "when only given a path argument" do
+      it "must return the path argument" do
+        expect(subject.request_uri(path)).to eq(path)
+      end
+    end
+
+    context "when given a path and a query: keyword argument" do
+      let(:query) { "id=1" }
+
+      it "must join the two usin a '?' character" do
+        expect(subject.request_uri(path, query: query)).to eq(
+          "#{path}?#{query}"
+        )
+      end
+
+      context "when the path already contains a '?' character" do
+        let(:path) { "/foo?bar=2" }
+
+        it "must append the query string to the path using a '&' character" do
+          expect(subject.request_uri(path, query: query)).to eq(
+            "#{path}&#{query}"
+          )
+        end
+      end
+    end
+
+    context "when given a path and a query_params: keyword argument" do
+      let(:query_params) do
+        {id: 1, bar: 2}
+      end
+      let(:query_string) { "id=1&bar=2" }
+
+      it "must convert the query_params into a String and add it to the path" do
+        expect(subject.request_uri(path, query_params: query_params)).to eq(
+          "#{path}?#{query_string}"
+        )
+      end
+
+      context "when the path already contains a '?' character" do
+        let(:path) { "/foo?bar=2" }
+
+        it "must append the query params to the path using a '&' character" do
+          expect(subject.request_uri(path, query_params: query_params)).to eq(
+            "#{path}&#{query_string}"
+          )
+        end
+      end
+    end
+  end
+
+  describe "METHODS" do
+    subject { described_class::METHODS }
+
+    describe ":copy" do
+      it "must map to Net::HTTP::Copy" do
+        expect(subject[:copy]).to be(Net::HTTP::Copy)
+      end
+    end
+
+    describe ":delete" do
+      it "must map to Net::HTTP::Delete" do
+        expect(subject[:delete]).to be(Net::HTTP::Delete)
+      end
+    end
+
+    describe ":get" do
+      it "must map to Net::HTTP::Get" do
+        expect(subject[:get]).to be(Net::HTTP::Get)
+      end
+    end
+
+    describe ":head" do
+      it "must map to Net::HTTP::Head" do
+        expect(subject[:head]).to be(Net::HTTP::Head)
+      end
+    end
+
+    describe ":lock" do
+      it "must map to Net::HTTP::Lock" do
+        expect(subject[:lock]).to be(Net::HTTP::Lock)
+      end
+    end
+
+    describe ":mkcol" do
+      it "must map to Net::HTTP::Mkcol" do
+        expect(subject[:mkcol]).to be(Net::HTTP::Mkcol)
+      end
+    end
+
+    describe ":move" do
+      it "must map to Net::HTTP::Move" do
+        expect(subject[:move]).to be(Net::HTTP::Move)
+      end
+    end
+
+    describe ":options" do
+      it "must map to Net::HTTP::Options" do
+        expect(subject[:options]).to be(Net::HTTP::Options)
+      end
+    end
+
+    describe ":patch" do
+      it "must map to Net::HTTP::Patch" do
+        expect(subject[:patch]).to be(Net::HTTP::Patch)
+      end
+    end
+
+    describe ":post" do
+      it "must map to Net::HTTP::Post" do
+        expect(subject[:post]).to be(Net::HTTP::Post)
+      end
+    end
+
+    describe ":propfind" do
+      it "must map to Net::HTTP::Propfind" do
+        expect(subject[:propfind]).to be(Net::HTTP::Propfind)
+      end
+    end
+
+    describe ":proppatch" do
+      it "must map to Net::HTTP::Proppatch" do
+        expect(subject[:proppatch]).to be(Net::HTTP::Proppatch)
+      end
+    end
+
+    describe ":put" do
+      it "must map to Net::HTTP::Put" do
+        expect(subject[:put]).to be(Net::HTTP::Put)
+      end
+    end
+
+    describe ":trace" do
+      it "must map to Net::HTTP::Trace" do
+        expect(subject[:trace]).to be(Net::HTTP::Trace)
+      end
+    end
+
+    describe ":unlock" do
+      it "must map to Net::HTTP::Unlock" do
+        expect(subject[:unlock]).to be(Net::HTTP::Unlock)
+      end
+    end
+  end
+
+  describe ".build" do
+    let(:path) { '/foo' }
+
+    it "must return a Net::HTTPRequest object with the #path set" do
+      request = subject.build(:get, path)
+
+      expect(request).to be_kind_of(Net::HTTPRequest)
+      expect(request.path).to eq(path)
+    end
+
+    context "when given a method argument of :copy" do
+      subject { super().build(:copy, '/') }
+
+      it "must create HTTP Copy requests" do
+        expect(subject.class).to eq(Net::HTTP::Copy)
+      end
+    end
+
+    context "when given a method argument of :delete" do
+      subject { super().build(:delete, '/') }
+
+      it "must create HTTP Delete requests" do
+        expect(subject.class).to eq(Net::HTTP::Delete)
+      end
+    end
+
+    context "when given a method argument of :get" do
+      subject { super().build(:get, '/') }
+
+      it "must create HTTP Get requests" do
+        expect(subject.class).to eq(Net::HTTP::Get)
+      end
+    end
+
+    context "when given a method argument of :head" do
+      subject { super().build(:head, '/') }
+
+      it "must create HTTP Head requests" do
+        expect(subject.class).to eq(Net::HTTP::Head)
+      end
+    end
+
+    context "when given a method argument of :lock" do
+      subject { super().build(:lock, '/') }
+
+      it "must create HTTP Lock requests" do
+        expect(subject.class).to eq(Net::HTTP::Lock)
+      end
+    end
+
+    context "when given a method argument of :mkcol" do
+      subject { super().build(:mkcol, '/') }
+
+      it "must create HTTP Mkcol requests" do
+        expect(subject.class).to eq(Net::HTTP::Mkcol)
+      end
+    end
+
+    context "when given a method argument of :move" do
+      subject { super().build(:move, '/') }
+
+      it "must create HTTP Move requests" do
+        expect(subject.class).to eq(Net::HTTP::Move)
+      end
+    end
+
+    context "when given a method argument of :options" do
+      subject { super().build(:options, '/') }
+
+      it "must create HTTP Options requests" do
+        expect(subject.class).to eq(Net::HTTP::Options)
+      end
+    end
+
+    context "when given a method argument of :post" do
+      subject { super().build(:post, '/') }
+
+      it "must create HTTP Post requests" do
+        expect(subject.class).to eq(Net::HTTP::Post)
+      end
+    end
+
+    context "when given a method argument of :propfind" do
+      subject { super().build(:propfind, '/') }
+
+      it "must create HTTP Propfind requests" do
+        expect(subject.class).to eq(Net::HTTP::Propfind)
+      end
+    end
+
+    context "when given a method argument of :proppatch" do
+      subject { super().build(:proppatch, '/') }
+
+      it "must create HTTP Proppatch requests" do
+        expect(subject.class).to eq(Net::HTTP::Proppatch)
+      end
+    end
+
+    context "when given a method argument of :trace" do
+      subject { super().build(:trace, '/') }
+
+      it "must create HTTP Trace requests" do
+        expect(subject.class).to eq(Net::HTTP::Trace)
+      end
+    end
+
+    context "when given a method argument of :unlock" do
+      subject { super().build(:unlock, '/') }
+
+      it "must create HTTP Unlock requests" do
+        expect(subject.class).to eq(Net::HTTP::Unlock)
+      end
+    end
+
+    context "when given an unknown method name" do
+      let(:method) { :foo }
+
+      it do
+        expect {
+          subject.build(method, '/')
+        }.to raise_error(ArgumentError,"unknown HTTP request method: #{method.inspect}")
+      end
+    end
+
+    context "when given the query: keyword argument" do
+      let(:query) { 'q=1' }
+
+      it "must append the query-string to the path" do
+        req = subject.build(:get,path, query: query)
+
+        expect(req.path).to eq("#{path}?#{query}")
+      end
+
+      context "when path already contains a query string" do
+        let(:additional_query) { 'x=2' }
+
+        it "must append the query using a '&' character" do
+          req = subject.build(:get,"#{path}?#{query}", query: additional_query)
+
+          expect(req.path).to eq("#{path}?#{query}&#{additional_query}")
+        end
+
+        context "when :query is empty" do
+          it "must append an extra '&'" do
+            req = subject.build(:get,"#{path}?#{query}", query:  '')
+
+            expect(req.path).to be_end_with('&')
+          end
+        end
+      end
+
+      context "when :query is empty" do
+        it "must append an extra '?'" do
+          req = subject.build(:get,path, query:  '')
+
+          expect(req.path).to be_end_with('?')
+        end
+      end
+    end
+
+    context "when given the headers: keyword argument" do
+      let(:header_name1)  { 'X-Foo' }
+      let(:header_value1) { 'foo'   }
+      let(:header_name2)  { 'X-Bar' }
+      let(:header_value2) { 'bar'   }
+
+      let(:headers) do
+        {
+          header_name1 => header_value1,
+          header_name2 => header_value2
+        }
+      end
+
+      it "must add the headers to the request" do
+        req = subject.build(:get,path, headers: headers)
+
+        expect(req[header_name1]).to eq(header_value1)
+        expect(req[header_name2]).to eq(header_value2)
+      end
+    end
+
+    context "when given the query_params: keyword argument" do
+      let(:query_params) do
+        {id: 1, bar: 2}
+      end
+      let(:query_string) { "id=1&bar=2" }
+
+      it "must convert the query_params into a String and add it to the path" do
+        req = subject.build(:get,path, query_params: query_params)
+
+        expect(req.path).to eq("#{path}?#{query_string}")
+      end
+
+      context "when the path already contains a '?' character" do
+        let(:path) { "/foo?bar=2" }
+
+        it "must append the query params to the path using a '&' character" do
+          req = subject.build(:get,path, query_params: query_params)
+
+          expect(req.path).to eq("#{path}&#{query_string}")
+        end
+      end
+    end
+
+    context "when given the user: keyword argument" do
+      it "must accept the :user option for Basic-Auth" do
+        req = subject.build(:get,path, user: 'joe')
+
+        expect(req['authorization']).to eq("Basic am9lOg==")
+      end
+
+      context "when also given the password: keyword argument" do
+        it "must also accept the :password options for Basic-Auth" do
+          req = subject.build(:get,path, user: 'joe', password: 'secret')
+
+          expect(req['authorization']).to eq("Basic am9lOnNlY3JldA==")
+        end
+      end
+    end
+
+    context "when given the :body keyword argument" do
+      context "and when it is a String" do
+        let(:body) { "foo bar baz" }
+
+        it "must set the request's #body" do
+          req = subject.build(:get,path, body: body)
+
+          expect(req.body).to be(body)
+        end
+      end
+
+      context "when the :body value is an IO object" do
+        let(:body) { File.new(__FILE__) }
+
+        it "must set the request's #body_stream" do
+          req = subject.build(:get,path, body: body)
+
+          expect(req.body_stream).to be(body)
+        end
+      end
+
+      context "when the :body value is an StringIO object" do
+        let(:body) { StringIO.new('foo bar baz') }
+
+        it "must set the request's #body_stream" do
+          req = subject.build(:get,path, body: body)
+
+          expect(req.body_stream).to be(body)
+        end
+      end
+    end
+
+    context "when given the form_data: keyword argument" do
+      subject { super().build(:post,'/', form_data: form_data) }
+
+      context "but it's a String value" do
+        let(:form_data) { "foo=1&bar=2" }
+
+        it "must set the content_type of the request to 'application/x-www-form-urlencoded'" do
+          expect(subject.content_type).to eq('application/x-www-form-urlencoded')
+        end
+
+        it "must set the body of the request" do
+          expect(subject.body).to eq(form_data)
+        end
+      end
+
+      context "and it's a Hash" do
+        let(:form_data) do
+          {'foo' => 1, 'bar' => 2}
+        end
+
+        it "must set the body of the request to the encoded form data" do
+          expect(subject.body).to eq(URI.encode_www_form(form_data))
+        end
+      end
+
+      context "and it's an Array of Arrays" do
+        let(:form_data) do
+          [['foo', 1], ['bar', 2]]
+        end
+
+        it "must set the body of the request to the encoded form data" do
+          expect(subject.body).to eq(URI.encode_www_form(form_data))
+        end
+      end
+    end
+  end
+end
