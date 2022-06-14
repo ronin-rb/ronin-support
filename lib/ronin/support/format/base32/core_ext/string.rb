562d73d28c9e4d5f59e93b48757b981f9ad4a86c
Postmodern <postmodern.mod3@gmail.com>
2022-06-14 15:01:05 -0700

    Added `String#base32_encode` and `String#base32_decode` (issue #101).
    * But do not require them by default, to avoid messing with tab-completion of
      `"...".base64_*`.

---
+++ lib/ronin/support/format/base32/core_ext/string.rb
@@ -0,0 +1,60 @@
+#
+# Copyright (c) 2006-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of ronin-support.
+#
+# ronin-support is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-support is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-support.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/support/base32'
+
+class String
+
+  #
+  # Base32 encodes the String.
+  #
+  # @return [String]
+  #   The Base32 encoded String.
+  #
+  # @example
+  #   "The quick brown fox jumps over the lazy dog".base32_encode
+  #   # => "KRUGKIDROVUWG2ZAMJZG653OEBTG66BANJ2W24DTEBXXMZLSEB2GQZJANRQXU6JAMRXWO==="
+  #
+  # @api public
+  #
+  # @since 1.0.0
+  #
+  def base32_encode
+    Ronin::Support::Base32.encode(self)
+  end
+
+  #
+  # Base32 decodes the String.
+  #
+  # @return [String]
+  #   The Base32 decodes String.
+  #
+  # @example
+  #   "KRUGKIDROVUWG2ZAMJZG653OEBTG66BANJ2W24DTEBXXMZLSEB2GQZJANRQXU6JAMRXWO===".base32_decode
+  #   # => "The quick brown fox jumps over the lazy dog"
+  #
+  # @api public
+  #
+  # @since 1.0.0
+  #
+  def base32_decode
+    Ronin::Support::Base32.decode(self)
+  end
+
+end
