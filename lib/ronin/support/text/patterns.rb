2a64120dbf538ebb8ac6cd152f63c09eef6c5b39
Postmodern <postmodern.mod3@gmail.com>
2022-06-13 23:26:25 -0700

    Added look-ahead to `DECIMAL_OCTET` to prevent partial matching of numbers.

--- lib/ronin/support/text/patterns.rb
+++ lib/ronin/support/text/patterns.rb
@@ -112,7 +112,7 @@ module Ronin
         # Regular expression for finding a decimal octet (0 - 255)
         #
         # @since 0.4.0
-        DECIMAL_OCTET = /25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9]/
+        DECIMAL_OCTET = /(?<=[^\d]|^)(?:25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])(?=[^\d]|$)/
 
         # Regular expression for finding MAC addresses in text
         #
