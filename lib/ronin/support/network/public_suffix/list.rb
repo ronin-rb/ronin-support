5fcdb24df56889a71fd320d51fc6a0db7c583639
Postmodern <postmodern.mod3@gmail.com>
2022-06-20 20:40:12 -0700

    Added `Network::PublicSuffix::List` (closes #323).

---
+++ lib/ronin/support/network/public_suffix/list.rb
@@ -0,0 +1,324 @@
+#
+# Copyright (c) 2006-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of ronin-support.
+#
+# ronin-support is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-support is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-support.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/support/home'
+
+require 'net/https'
+
+module Ronin
+  module Support
+    module Network
+      module PublicSuffix
+        #
+        # Represents the [public suffix list].
+        #
+        # [public suffix list]: https://publicsuffix.org/
+        #
+        # @api public
+        #
+        # @since 1.0.0
+        #
+        class List
+
+          include Enumerable
+
+          # File name of the public suffix list.
+          FILE_NAME = 'public_suffix_list.dat'
+
+          # The `https://publicsuffix.org/list/public_suffix_list.dat` URL.
+          URL = "https://publicsuffix.org/list/#{FILE_NAME}"
+
+          # The path to `~/.local/share/ronin/ronin-support/public_suffix_list.dat` list file.
+          PATH = File.join(Home::LOCAL_SHARE_DIR,'ronin','ronin-support',FILE_NAME)
+
+          # The path to the list file.
+          #
+          # @return [String]
+          attr_reader :path
+
+          # The list of all public suffixes.
+          #
+          # @return [Array<String>]
+          attr_reader :list
+
+          # The tree of all public suffix TLDs.
+          #
+          # @return [Hash{String => Hash}]
+          attr_reader :tree
+
+          #
+          # Initializes the public suffix list.
+          #
+          # @param [String] path
+          #   The path to the list file.
+          #
+          # @api private
+          #
+          def initialize(path=PATH)
+            @path = path
+
+            @list = []
+            @tree = {}
+          end
+
+          #
+          # Determines whether the list file has been previously downloaded.
+          #
+          # @param [String] path
+          #   An optional alternate path to the list file.
+          #
+          # @return [Boolean]
+          #
+          def self.downloaded?(path=PATH)
+            File.file?(path)
+          end
+
+          # One day in seconds.
+          ONE_DAY = 60 * 60 * 24
+
+          #
+          # Determines if the downloaded list file is older than one day.
+          #
+          # @param [String] path
+          #   An optional alternate path to the list file.
+          #
+          # @return [Boolean]
+          #
+          def self.stale?(path=PATH)
+            !File.file?(path) || File.stat(path).mtime < (Time.now - ONE_DAY)
+          end
+
+          #
+          # Downloads the list file.
+          #
+          # @param [String] url
+          #   An optional alternate URL to download the `ip2asn-combined.tsv.gz`
+          #   file.
+          #
+          # @param [String] path
+          #   An optional alternate path to the list file.
+          #
+          def self.download(url: URL, path: PATH)
+            uri = URI(url)
+
+            Net::HTTP.start(uri.host,uri.port, use_ssl: true) do |http|
+              request  = Net::HTTP::Get.new(uri.path)
+
+              http.request(request) do |response|
+                File.open("#{path}.part",'wb') do |file|
+                  response.read_body do |chunk|
+                    file.write(chunk)
+                  end
+                end
+
+                FileUtils.mv("#{path}.part",path)
+              end
+            end
+          end
+
+          #
+          # Optionally update the cached list file if it is older than one day.
+          #
+          # @param [String] url
+          #   An optional alternate URL to download the `ip2asn-combined.tsv.gz`
+          #   file.
+          #
+          # @param [String] path
+          #   An optional alternate path to the list file.
+          #
+          def self.update(url: URL, path: PATH)
+            if !downloaded?(path)
+              download(url: url, path: path)
+            elsif stale?(path)
+              begin
+                download(url: url, path: path)
+              rescue
+              end
+            end
+          end
+
+          #
+          # Loads the public suffix list from the given file.
+          #
+          # @param [String] path
+          #   The path to the public suffix list file.
+          #
+          # @return [List]
+          #   The parsed public suffix list file.
+          #
+          def self.load_file(path=PATH)
+            list = new(path)
+
+            File.open(path) do |file|
+              file.each_line(chomp: true) do |line|
+                next if (line.empty? || line.start_with?('//'))
+
+                list << line
+              end
+            end
+
+            return list
+          end
+
+          #
+          # Adds a public suffix to the list.
+          #
+          # @param [String] suffix
+          #   The suffix String to add.
+          #
+          # @return [self]
+          #
+          # @api private
+          #
+          def <<(suffix)
+            @list << suffix
+
+            if suffix.include?('.')
+              tree = @tree
+
+              suffix.split('.').reverse_each.each_cons(2) do |parent,child|
+                subtree = tree[parent] ||= {}
+                subtree[child] ||= nil
+
+                tree = subtree
+              end
+            else
+              @tree[suffix] ||= nil
+            end
+
+            return self
+          end
+
+          #
+          # Enumerates over each suffix in the list.
+          #
+          # @yield [suffix]
+          #   If a block is given, it will be passed each suffix in the list.
+          #
+          # @yieldparam [String] suffix
+          #   A domain suffix in the list.
+          #
+          # @return [Enumerator]
+          #   If no block is given, an Enumerator object will be returned.
+          #
+          def each(&block)
+            @list.each(&block)
+          end
+
+          #
+          # Splits a host name into it's name and tld components.
+          #
+          # @param [String] host_name
+          #   The host name to split.
+          #
+          # @return [(String, String)]
+          #   The host name's name and tld components.
+          #
+          def split(host_name)
+            host_name = host_name.dup
+            suffix    = ""
+
+            tree = @tree
+
+            while tree
+              name = if (index = host_name.rindex('.')) then host_name[index..]
+                     else                                    host_name
+                     end
+
+              tld, subtree = tree.find do |tld,subtree|
+                tld == '*' || name == ".#{tld}"
+              end
+
+              if tld
+                host_name.chomp!(name)
+                suffix.prepend(name)
+              end
+
+              tree = subtree
+            end
+
+            return host_name, suffix unless suffix.empty?
+          end
+
+          #
+          # Creates a regular expression that can match every domain suffix in
+          # the list.
+          #
+          # @return [Regexp]
+          #
+          def to_regexp
+            regexp = Regexp.union(@tree.map { |tld,subtree|
+              tld_regexp(tld,subtree)
+            })
+
+            return /(?<=\.|^)#{regexp}(?=[^\.a-z0-9-]|$)/
+          end
+
+          #
+          # Inspects the public suffix list.
+          #
+          # @return [String]
+          #   The inspected list object.
+          #
+          def inspect
+            "#<#{self.class}: #{@path}>"
+          end
+
+          private
+
+          #
+          # Create a regexp to match the given Top-Level-Domain (TLD) and all
+          # sub-TLDs below it.
+          #
+          # @param [String] tld
+          #   The Top-Level-Domain (TLD).
+          #
+          # @param [Hash{String => Hash}] subtree
+          #   The other TLDs below the given TLD.
+          #
+          # @return [Regexp]
+          #   The compiled regular expression.
+          #
+          def tld_regexp(tld,subtree)
+            if subtree
+              subtree_regexp = if subtree.length == 1
+                                  tld_regexp(subtree.keys[0],subtree.values[0])
+                                else
+                                  Regexp.union(
+                                    subtree.map { |sub_tld,sub_subtree|
+                                      tld_regexp(sub_tld,sub_subtree)
+                                    }
+                                  )
+                                end
+
+              /(?:#{subtree_regexp}\.)?#{tld}/
+            else
+              if tld == '*'
+                /[a-z0-9]+(?:-[a-z0-9]+)*/
+              else
+                tld
+              end
+            end
+          end
+
+        end
+      end
+    end
+  end
+end
---
+++ spec/network/public_suffix/list_spec.rb
@@ -0,0 +1,462 @@
+require 'spec_helper'
+require 'ronin/support/network/public_suffix/list'
+
+require 'fileutils'
+require 'tmpdir'
+
+describe Ronin::Support::Network::PublicSuffix::List do
+  let(:fixtures_dir) { File.join(__dir__,'fixtures') }
+  let(:list_file)    { File.join(fixtures_dir,'public_suffix_list.dat') }
+  let(:path)         { list_file }
+
+  describe "FILE_NAME" do
+    subject { described_class::FILE_NAME }
+
+    it do
+      expect(subject).to eq('public_suffix_list.dat')
+    end
+  end
+
+  describe "URL" do
+    subject { described_class::URL }
+
+    it do
+      expect(subject).to eq('https://publicsuffix.org/list/public_suffix_list.dat')
+    end
+  end
+
+  describe "PATH" do
+    subject { described_class::PATH }
+
+    it do
+      expect(subject).to eq(File.join(Gem.user_home,'.local','share','ronin','ronin-support','public_suffix_list.dat'))
+    end
+  end
+
+  describe "file methods" do
+    let(:tempdir) { Dir.mktmpdir('ronin-support') }
+    let(:path)    { File.join(tempdir,described_class::FILE_NAME) }
+
+    before do
+      stub_const("#{described_class}::PATH",path)
+    end
+
+    describe ".downloaded?" do
+      subject { described_class }
+
+      context "when given no argument" do
+        context "when the file does not exist" do
+          it "must return false" do
+            expect(subject.downloaded?).to be(false)
+          end
+        end
+
+        context "when the file does exist" do
+          before { FileUtils.touch(path) }
+
+          it "must return true" do
+            expect(subject.downloaded?).to be(true)
+          end
+
+          after { FileUtils.rm(path) }
+        end
+      end
+
+      context "when given a path" do
+        context "when the file does not exist" do
+          it "must return false" do
+            expect(subject.downloaded?(path)).to be(false)
+          end
+        end
+
+        context "when the file does exist" do
+          before { FileUtils.touch(path) }
+
+          it "must return true" do
+            expect(subject.downloaded?(path)).to be(true)
+          end
+
+          after { FileUtils.rm(path) }
+        end
+      end
+    end
+
+    describe ".stale?" do
+      subject { described_class }
+
+      context "when given no argument" do
+        context "when the file does not exist" do
+          it "must return true" do
+            expect(subject.stale?).to be(true)
+          end
+        end
+
+        context "when the file does exist" do
+          before { FileUtils.touch(path) }
+
+          context "and the modification time of the file is within the last day" do
+            before do
+              FileUtils.touch(path, mtime: (Time.now - (60 * 60)))
+            end
+
+            it "must return false" do
+              expect(subject.stale?).to be(false)
+            end
+          end
+
+          context "but the modification time is older than one day" do
+            before do
+              FileUtils.touch(path, mtime: (Time.now - (60 * 60 * 25)))
+            end
+
+            it "must return true" do
+              expect(subject.stale?).to be(true)
+            end
+          end
+
+          after { FileUtils.rm(path) }
+        end
+      end
+
+      context "when given a path" do
+        context "when the file does not exist" do
+          it "must return true" do
+            expect(subject.stale?(path)).to be(true)
+          end
+        end
+
+        context "when the file does exist" do
+          before { FileUtils.touch(path) }
+
+          context "and the modification time of the file is within the last day" do
+            before do
+              FileUtils.touch(path, mtime: (Time.now - (60 * 60)))
+            end
+
+            it "must return false" do
+              expect(subject.stale?(path)).to be(false)
+            end
+          end
+
+          context "but the modification time is older than one day" do
+            before do
+              FileUtils.touch(path, mtime: (Time.now - (60 * 60 * 25)))
+            end
+
+            it "must return true" do
+              expect(subject.stale?(path)).to be(true)
+            end
+          end
+
+          after { FileUtils.rm(path) }
+        end
+      end
+    end
+
+    describe ".download", :network do
+      subject { described_class }
+
+      context "when given no keyword arguments" do
+        before { subject.download }
+
+        it "must download the URL to the PATH" do
+          expect(File.file?(described_class::PATH)).to be(true)
+          expect(File.stat(described_class::PATH).size).to be > 0
+        end
+      end
+    end
+
+    describe ".update" do
+      subject { described_class }
+
+      context "when given no keyword arguments" do
+        context "and when .downloaded? returns false" do
+          before { allow(subject).to receive(:downloaded?).and_return(false) }
+
+          it "must call .download" do
+            expect(subject).to receive(:download)
+
+            subject.update
+          end
+
+          context "but when .download raises an exception" do
+            let(:exception) { RuntimeError.new("download failed") }
+
+            it "must ignore any exceptions" do
+              allow(subject).to receive(:download).and_raise(exception)
+
+              expect {
+                subject.update
+              }.to raise_error(exception)
+            end
+          end
+        end
+
+        context "and when .downloaded? returns true" do
+          before { allow(subject).to receive(:downloaded?).and_return(true) }
+
+          context "and when .stale? returns true" do
+            before { allow(subject).to receive(:stale?).and_return(true) }
+
+            it "must call .download" do
+              expect(subject).to receive(:download)
+
+              subject.update
+            end
+
+            context "but when .download raises an exception" do
+              it "must ignore any exceptions" do
+                allow(subject).to receive(:download).and_raise(
+                  RuntimeError.new("download failed")
+                )
+
+                expect {
+                  subject.update
+                }.to_not raise_error
+              end
+            end
+          end
+
+          context "and when .stale? returns false" do
+            before { allow(subject).to receive(:stale?).and_return(false) }
+
+            it "must not call .download" do
+              expect(subject).to_not receive(:download)
+
+              subject.update
+            end
+          end
+        end
+      end
+    end
+
+    describe ".load_file" do
+      let(:path) { list_file }
+
+      subject { described_class }
+
+      context "when given no argument" do
+        let(:path) { File.join(fixtures_dir,'public_suffix_list.dat') }
+
+        subject { super().load_file }
+
+        it "must return a #{described_class} object with the parsed records" do
+          expect(subject).to be_kind_of(described_class)
+          expect(subject.list).to_not be_empty
+          expect(subject.tree).to_not be_empty
+        end
+      end
+
+      context "when given a path" do
+        subject { super().load_file(path) }
+
+        it "must return a #{described_class} object with the parsed records" do
+          expect(subject).to be_kind_of(described_class)
+          expect(subject.list).to_not be_empty
+          expect(subject.tree).to_not be_empty
+        end
+      end
+    end
+  end
+
+  describe "#initialize" do
+    subject { described_class.new }
+
+    it "must default #path to #{described_class}::PATH" do
+      expect(subject.path).to eq(described_class::PATH)
+    end
+
+    context "when given a custom path" do
+      let(:path) { '/path/to/public_suffix_list.dat' }
+
+      subject { described_class.new(path) }
+
+      it "must set #path" do
+        expect(subject.path).to eq(path)
+      end
+    end
+
+    it "must initializes #list to []" do
+      expect(subject.list).to eq([])
+    end
+
+    it "must initializes #tree to {}" do
+      expect(subject.tree).to eq({})
+    end
+  end
+
+  describe "#<<" do
+    subject { described_class.new }
+
+    let(:suffix1) { 'arpa'     }
+    let(:suffix2) { 'ip6.arpa' }
+
+    it "must append the suffix to the #list" do
+      subject << suffix1
+      subject << suffix2
+
+      expect(subject.list).to eq([suffix1, suffix2])
+    end
+
+    context "when the suffix does not contain a '.'" do
+      let(:tld) { "com" }
+
+      before { subject << tld }
+
+      it "must add the TLD to #tree as a key" do
+        expect(subject.tree).to have_key(tld)
+      end
+    end
+
+    context "when the suffix does contain a '.'" do
+      let(:tld)     { "uk" }
+      let(:sub_tld) { "co" }
+      let(:suffix)  { "#{sub_tld}.#{tld}" }
+
+      before { subject << suffix }
+
+      it "must add the first tld to #tree as a key" do
+        expect(subject.tree).to have_key(tld)
+      end
+
+      it "must add a sub-Hash to #tree using the TLD as a key" do
+        expect(subject.tree[tld]).to be_kind_of(Hash)
+      end
+
+      it "must add the second TLD to the TLD's sub-Hash as a key" do
+        expect(subject.tree[tld]).to eq({sub_tld => nil})
+      end
+    end
+
+    context "when the suffix does contain two '.' characters" do
+      let(:tld)         { "org" }
+      let(:sub_tld)     { "dyndns" }
+      let(:sub_sub_tld) { "go" }
+      let(:suffix)      { "#{sub_sub_tld}.#{sub_tld}.#{tld}" }
+
+      before { subject << suffix }
+
+      it "must add the first tld to #tree as a key" do
+        expect(subject.tree).to have_key(tld)
+      end
+
+      it "must add a sub-Hash to #tree using the TLD as a key" do
+        expect(subject.tree[tld]).to be_kind_of(Hash)
+      end
+
+      it "must add the second TLD to the sub-Hash as a key" do
+        expect(subject.tree[tld]).to have_key(sub_tld)
+      end
+
+      it "must add a sub-sub-Hash to #tree using the second TLD as a key" do
+        expect(subject.tree[tld][sub_tld]).to be_kind_of(Hash)
+      end
+
+      it "must add the third TLD to the sub-sub-Hash as a key" do
+        expect(subject.tree[tld][sub_tld]).to eq({sub_sub_tld => nil})
+      end
+    end
+  end
+
+  subject { described_class.load_file(path) }
+
+  describe "#each" do
+    context "when given a block" do
+      it "must yield every entry in #list" do
+        expect { |b|
+          subject.each(&b)
+        }.to yield_successive_args(*subject.list)
+      end
+    end
+
+    context "when given no block" do
+      it "must return an Enumerator for #each" do
+        expect(subject.each.to_a).to eq(subject.list)
+      end
+    end
+  end
+
+  describe "#split" do
+    context "when given a domain name" do
+      context "when the domain ends with a single TLD" do
+        let(:prefix) { "example" }
+        let(:tld)    { "com"     }
+        let(:domain) { "#{prefix}.#{tld}" }
+
+        it "must return the domain prefix and the TLD" do
+          expect(subject.split(domain)).to eq([prefix, ".#{tld}"])
+        end
+      end
+
+      context "when the domain ends with multiple TLDs" do
+        let(:prefix) { "example" }
+        let(:suffix) { "co.uk"   }
+        let(:domain) { "#{prefix}.#{suffix}" }
+
+        it "must return the domain prefix and the suffix" do
+          expect(subject.split(domain)).to eq([prefix, ".#{suffix}"])
+        end
+      end
+
+      context "when the public suffix starts with a '*' wildcard" do
+        let(:prefix) { "example" }
+        let(:label)  { 'foo'     }
+        let(:suffix) { "nom.br"  }
+        let(:domain) { "#{prefix}.#{label}.#{suffix}" }
+
+        it "must consume an arbitrary label for the '*' wildcard label" do
+          expect(subject.split(domain)).to eq([prefix, ".#{label}.#{suffix}"])
+        end
+      end
+    end
+
+    context "when given a hostname" do
+      context "when the hostname ends with a single TLD" do
+        let(:prefix)   { "www.example" }
+        let(:tld)      { "com"         }
+        let(:hostname) { "#{prefix}.#{tld}" }
+
+        it "must return the hostname label and the TLD" do
+          expect(subject.split(hostname)).to eq([prefix, ".#{tld}"])
+        end
+      end
+
+      context "when the hostname ends with multiple TLDs" do
+        let(:prefix)   { "www.example" }
+        let(:suffix)   { "co.uk"       }
+        let(:hostname) { "#{prefix}.#{suffix}" }
+
+        it "must return the hostname label and the suffix" do
+          expect(subject.split(hostname)).to eq([prefix, ".#{suffix}"])
+        end
+      end
+
+      context "when the public suffix starts with a '*' wildcard" do
+        let(:prefix)   { "www.example" }
+        let(:label)    { 'foo'         }
+        let(:suffix)   { "nom.br"      }
+        let(:hostname) { "#{prefix}.#{label}.#{suffix}" }
+
+        it "must consume an arbitrary label for the '*' wildcard label" do
+          expect(subject.split(hostname)).to eq([prefix, ".#{label}.#{suffix}"])
+        end
+      end
+    end
+  end
+
+  describe "#to_regexp" do
+    it "must return a Regexp" do
+      expect(subject.to_regexp).to be_kind_of(Regexp)
+    end
+
+    it "must match every suffix in the #list" do
+      expect(subject.list).to all(match(subject.to_regexp))
+    end
+  end
+
+  describe "#inspect" do
+    it "must include the class name and the #path" do
+      expect(subject.inspect).to eq("#<#{described_class}: #{subject.path}>")
+    end
+  end
+end
