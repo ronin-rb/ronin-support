c2a31f5502dbe101b79a080fc25d1c698ee79323
Postmodern <postmodern.mod3@gmail.com>
2022-08-12 16:30:37 -0700

    Improved specs for `String#base64_encode` / `String#base64_decode`.

--- spec/encoding/base64/core_ext/string_spec.rb
+++ spec/encoding/base64/core_ext/string_spec.rb
@@ -10,18 +10,77 @@ describe String do
   it { expect(subject).to respond_to(:base64_decode) }
 
   describe "#base64_encode" do
-    subject { "hello\0" }
+    subject { "hello" }
 
-    it "must base64 encode a String" do
-      expect(subject.base64_encode).to eq("aGVsbG8A\n")
+    it "must Base64 encode the String" do
+      expect(subject.base64_encode).to eq(Base64.encode64(subject))
+    end
+
+    context "when given the mode: keyword of :strict" do
+      subject { 'A' * 256 }
+
+      it "must strict encode the String" do
+        expect(subject.base64_encode(mode: :strict)).to eq(
+          Base64.strict_encode64(subject)
+        )
+      end
+    end
+
+    context "when given the mode: keyword of :url_safe" do
+      subject { 'A' * 256 }
+
+      it "must URL-safe encode the String" do
+        expect(subject.base64_encode(mode: :url_safe)).to eq(
+          Base64.strict_encode64(subject)
+        )
+      end
+    end
+
+    context "when given any other value" do
+      let(:mode) { :foo }
+
+      it do
+        expect {
+          subject.base64_encode(mode: mode)
+        }.to raise_error(ArgumentError,"Base64 mode must be either :string, :url, or nil: #{mode.inspect}")
+      end
     end
   end
 
   describe "#base64_decode" do
-    subject { "aGVsbG8A\n" }
+    let(:data)    { "hello" }
+    let(:subject) { Base64.encode64(data) }
 
-    it "must base64 decode a String" do
-      expect(subject.base64_decode).to eq("hello\0")
+    it "must Base64 decode the given data" do
+      expect(subject.base64_decode).to eq(data)
+    end
+
+    context "when given the mode: keyword of :strict" do
+      let(:data)    { 'A' * 256 }
+      let(:subject) { Base64.strict_encode64(data) }
+
+      it "must strict decode the given data" do
+        expect(subject.base64_decode(mode: :strict)).to eq(data)
+      end
+    end
+
+    context "when given the mode: keyword of :url_safe" do
+      let(:data)    { 'A' * 256 }
+      let(:subject) { Base64.urlsafe_encode64(data) }
+
+      it "must URL-safe decode the given data" do
+        expect(subject.base64_decode(mode: :url_safe)).to eq(data)
+      end
+    end
+
+    context "when given any other value" do
+      let(:mode) { :foo }
+
+      it do
+        expect {
+          subject.base64_decode(mode: mode)
+        }.to raise_error(ArgumentError,"Base64 mode must be either :string, :url, or nil: #{mode.inspect}")
+      end
     end
   end
 end
