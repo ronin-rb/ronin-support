40e61ac241be26fd15ee0ccea7815e1fb6a5cb28
Postmodern <postmodern.mod3@gmail.com>
2022-08-12 23:41:53 -0700

    Change order of specs.

--- spec/encoding/shell/core_ext/integer_spec.rb
+++ spec/encoding/shell/core_ext/integer_spec.rb
@@ -12,8 +12,36 @@ describe Integer do
   it { expect(subject).to respond_to(:sh_escape) }
   it { expect(subject).to respond_to(:bash_escape) }
 
+  describe "#shell_encode" do
+    context "when called on an Integer between 0x00 and 0xff" do
+      subject { 0x41 }
+
+      it "must encode the Integer as a shell hex character" do
+        expect(subject.shell_encode).to eq("\\x41")
+      end
+    end
+
+    context "when called on an Integer is greater than 0xff" do
+      subject { 0xFFFF }
+
+      it "must return the lowercase \\uXXXX hex escaped String" do
+        expect(subject.shell_encode).to eq("\\uffff")
+      end
+    end
+
+    context "when called on a negative Integer" do
+      subject { -1 }
+
+      it do
+        expect {
+          subject.shell_encode
+        }.to raise_error(RangeError,"#{subject} out of char range")
+      end
+    end
+  end
+
   describe "#shell_escape" do
-    described_class::SHELL_ESCAPE_BYTES.each do |byte,escaped_char|
+    Ronin::Support::Encoding::Shell::ESCAPE_BYTES.each do |byte,escaped_char|
       context "when called on #{byte}" do
         subject { byte }
 
@@ -53,34 +81,6 @@ describe Integer do
       it do
         expect {
           subject.shell_escape
-        }.to raise_error(RangeError,"#{subject} out of char range")
-      end
-    end
-  end
-
-  describe "#shell_encode" do
-    context "when called on an Integer between 0x00 and 0xff" do
-      subject { 0x41 }
-
-      it "must encode the Integer as a shell hex character" do
-        expect(subject.shell_encode).to eq("\\x41")
-      end
-    end
-
-    context "when called on an Integer is greater than 0xff" do
-      subject { 0xFFFF }
-
-      it "must return the lowercase \\uXXXX hex escaped String" do
-        expect(subject.shell_encode).to eq("\\uffff")
-      end
-    end
-
-    context "when called on a negative Integer" do
-      subject { -1 }
-
-      it do
-        expect {
-          subject.shell_encode
         }.to raise_error(RangeError,"#{subject} out of char range")
       end
     end
