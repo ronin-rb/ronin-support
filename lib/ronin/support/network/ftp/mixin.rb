411405c2261ce1878e52fa3651f3980ed6644599
Postmodern <postmodern.mod3@gmail.com>
2022-08-17 20:17:05 -0700

    Combined `ftp_connect` and `ftp_session` (issue #61).

--- lib/ronin/support/network/ftp/mixin.rb
+++ lib/ronin/support/network/ftp/mixin.rb
@@ -61,12 +61,13 @@ module Ronin
           #
           # @yield [ftp]
           #   If a block is given, it will be passed an FTP session object.
+          #   Once the block returns, the FTP session will be closed.
           #
           # @yieldparam [Net::FTP] ftp
           #   The FTP session.
           #
-          # @return [Net::FTP]
-          #   The FTP session.
+          # @return [Net::FTP, nil]
+          #   The FTP session. If a block is given, then `nil` will be returned.
           #
           # @example
           #   ftp_connect('www.example.com', user: 'joe', password: 'secret')
@@ -85,57 +86,12 @@ module Ronin
             ftp.login(user,password,account)
             ftp.passive = passive
 
-            yield ftp if block_given?
-            return ftp
-          end
-
-          #
-          # Starts a session with the FTP server.
-          #
-          # @param [String] host
-          #   The host to connect to.
-          #
-          # @param [Hash{Symbol => Object}] kwargs
-          #   Additional keyword arguments for {#ftp_connect}.
-          #
-          # @option kwargs [Integer] :port (DEFAULT_PORT)
-          #   The port to connect to.
-          #
-          # @option kwargs [String] :user (DEFAULT_USER)
-          #   The user to authenticate with.
-          #
-          # @option kwargs [String] :password
-          #   The password to authenticate with.
-          #
-          # @option kwargs [String] :account
-          #   The FTP account information to send via the `ACCT` command.
-          #
-          # @option kwargs [Boolean] :passive (true)
-          #   Specifies whether the FTP session should use passive mode.
-          #
-          # @yield [ftp]
-          #   If a block is given, it will be passed an FTP session object.
-          #   After the block has returned, the session will be closed.
-          #
-          # @yieldparam [Net::FTP] ftp
-          #   The FTP session.
-          #
-          # @example
-          #   ftp_session('www.example.com', user: 'joe') do |ftp|
-          #     # ...
-          #   end
-          #
-          # @see Network::FTP#ftp_session
-          #
-          # @api public
-          #
-          def ftp_session(host,**kwargs)
-            ftp = ftp_connect(host,**kwargs)
-
-            yield ftp if block_given?
-
-            ftp.close
-            return nil
+            if block_given?
+              yield ftp
+              ftp.close
+            else
+              return ftp
+            end
           end
         end
       end
--- spec/network/ftp/mixin_spec.rb
+++ spec/network/ftp/mixin_spec.rb
@@ -43,40 +43,28 @@ describe Ronin::Support::Network::FTP::Mixin do
       end
 
       context "when given a block" do
-        it "must yield the new Net::FTP object" do
-          ftp = subject.ftp_connect(host) do |ftp|
-            expect(ftp).to be_kind_of(Net::FTP)
+        it "must yield a new Net::FTP object" do
+          yielded_ftp = nil
+
+          subject.ftp_connect(host) do |ftp|
+            yielded_ftp = ftp
           end
 
-          ftp.close
-        end
-      end
-    end
-  end
-
-  describe "#ftp_session" do
-    context "integration", :network do
-      it "must yield a new Net::FTP object" do
-        yielded_ftp = nil
-
-        subject.ftp_session(host) do |ftp|
-          yielded_ftp = ftp
+          expect(yielded_ftp).to be_kind_of(Net::FTP)
         end
 
-        expect(yielded_ftp).to be_kind_of(Net::FTP)
-      end
+        it "must close the FTP session after yielding it" do
+          session  = nil
+          was_open = nil
 
-      it "must close the FTP session after yielding it" do
-        session  = nil
-        was_open = nil
+          subject.ftp_connect(host) do |ftp|
+            session   = ftp
+            was_open  = !ftp.closed?
+          end
 
-        subject.ftp_session(host) do |ftp|
-          session   = ftp
-          was_open  = !ftp.closed?
+          expect(was_open).to be(true)
+          expect(session).to be_closed
         end
-
-        expect(was_open).to be(true)
-        expect(session).to be_closed
       end
     end
   end
