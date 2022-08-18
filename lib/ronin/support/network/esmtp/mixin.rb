76befcd8f8a2cba3ee1ced4ef0472f768efa89a7
Postmodern <postmodern.mod3@gmail.com>
2022-08-17 20:13:56 -0700

    Combined `esmtp_connect` with `estmp_session` (issue #61).

--- lib/ronin/support/network/esmtp/mixin.rb
+++ lib/ronin/support/network/esmtp/mixin.rb
@@ -77,79 +77,28 @@ module Ronin
           #   The password to authenticate with.
           #
           # @yield [esmtp]
-          #   If a block is given, it will be passed an ESMTP enabled session
-          #   object.
+          #   If a block is given, it will be passed an ESMTP enabled session.
+          #   Once the block returns the ESMTP session will be closed.
           #
           # @yieldparam [Net::SMTP] esmtp
           #   The ESMTP session.
           #
-          # @return [Net::SMTP]
-          #   The ESMTP enabled session.
+          # @return [Net::SMTP, nil]
+          #   The ESMTP enabled session. If a block is given, `nil` will be
+          #   returned.
           #
           # @api public
           #
           def esmtp_connect(host,**kwargs)
-            smtp = smtp_connect(host,**kwargs)
-            smtp.esmtp = true
-
-            yield smtp if block_given?
-            return smtp
-          end
-
-          #
-          # Starts an ESMTP session with the ESMTP enabled server.
-          #
-          # @param [String] host
-          #   The host to connect to.
-          #
-          # @param [Hash{Symbol => Object}] kwargs
-          #   Additional keyword arguments for {#smtp_session}.
-          #
-          # @option kwargs [Integer] :port (SMTP::DEFAULT_PORT)
-          #   The port to connect to.
-          #
-          # @option kwargs [Boolean, Hash] :ssl
-          #   Additional SSL options.
-          #
-          # @option :ssl [Boolean] :verify
-          #   Specifies that the SSL certificate should be verified.
-          #
-          # @option :ssl [String] :certs
-          #   The path to the file containing CA certs of the server.
-          #
-          # @option kwargs [String] :helo
-          #   The HELO domain.
-          #
-          # @option kwargs [Symbol] :auth
-          #   The type of authentication to use.
-          #   May be one of the following:
-          #
-          #   * `:login`
-          #   * `:plain`
-          #   * `:cram_md5`
-          #
-          # @option kwargs [String] :user
-          #   The user-name to authenticate with.
-          #
-          # @option kwargs [String] :password
-          #   The password to authenticate with.
-          #
-          # @yield [esmtp]
-          #   If a block is given, it will be passed an ESMTP enabled session
-          #   object. After the block has returned, the session will be closed.
-          #
-          # @yieldparam [Net::SMTP] esmtp
-          #   The ESMTP session.
-          #
-          # @see esmtp_connect
-          #
-          # @api public
-          #
-          def esmtp_session(host,**kwargs)
-            smtp_session(host,**kwargs) do |smtp|
+            if block_given?
+              smtp_connect(host,**kwargs) do |smtp|
+                smtp.esmtp = true
+                yield smtp
+              end
+            else
+              smtp = smtp_connect(host,**kwargs)
               smtp.esmtp = true
-
-              yield smtp if block_given?
+              return smtp
             end
           end
         end
