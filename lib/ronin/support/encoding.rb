f207da5fc811dd803664ec22e9b6c83f8210dfe0
Postmodern <postmodern.mod3@gmail.com>
2022-08-22 01:44:14 -0700

    Require `ronin/support/encoding/base32`.

--- lib/ronin/support/encoding.rb
+++ lib/ronin/support/encoding.rb
@@ -18,6 +18,7 @@
 #
 
 require 'ronin/support/encoding/core_ext'
+require 'ronin/support/encoding/base32'
 require 'ronin/support/encoding/base64'
 require 'ronin/support/encoding/hex'
 require 'ronin/support/encoding/c'
