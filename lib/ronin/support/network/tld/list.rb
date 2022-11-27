b6fbb2517b5477adb7d3b73999a4d4f882097d9b
Postmodern <postmodern.mod3@gmail.com>
2022-11-27 14:32:10 -0800

    Added `Network::TLD::List` (issue #385).

---
+++ lib/ronin/support/network/tld/list.rb
@@ -0,0 +1,238 @@
+#
+# Copyright (c) 2006-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-support is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-support is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-support.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/support/network/exceptions'
+require 'ronin/support/home'
+
+require 'net/https'
+require 'fileutils'
+
+module Ronin
+  module Support
+    module Network
+      module TLD
+        #
+        # Represents the [Top-Level Domains list].
+        #
+        # [Top-Level Domains list]: https://www.icann.org/resources/pages/tlds-2012-02-25-en
+        #
+        # @api public
+        #
+        # @since 1.0.0
+        #
+        class List
+
+          include Enumerable
+
+          # File name of the TLD list.
+          FILE_NAME = 'tlds-alpha-by-domain.txt'
+
+          # The `https://data.iana.org/TLD/tlds-alpha-by-domain.txt`
+          URL = "https://data.iana.org/TLD/#{FILE_NAME}"
+
+          # The path to `~/.cache/ronin/ronin-support/tlds-alpha-by-domain.txt` list file.
+          PATH = File.join(Home::CACHE_DIR,'ronin','ronin-support',FILE_NAME)
+
+          # The path to the list file.
+          #
+          # @return [String]
+          attr_reader :path
+
+          # The list of all TLDs.
+          #
+          # @return [Array<String>]
+          attr_reader :list
+
+          # The tree of all TLD TLDs.
+          #
+          # @return [Hash{String => Hash}]
+          attr_reader :tree
+
+          #
+          # Initializes the TLD list.
+          #
+          # @param [String] path
+          #   The path to the list file.
+          #
+          # @api private
+          #
+          def initialize(path=PATH)
+            @path = path
+
+            @list = []
+            @tree = {}
+          end
+
+          #
+          # Determines whether the list file has been previously downloaded.
+          #
+          # @param [String] path
+          #   An optional alternate path to the list file.
+          #
+          # @return [Boolean]
+          #
+          def self.downloaded?(path=PATH)
+            File.file?(path)
+          end
+
+          # One day in seconds.
+          ONE_DAY = 60 * 60 * 24
+
+          #
+          # Determines if the downloaded list file is older than one day.
+          #
+          # @param [String] path
+          #   An optional alternate path to the list file.
+          #
+          # @return [Boolean]
+          #
+          def self.stale?(path=PATH)
+            !File.file?(path) || File.stat(path).mtime < (Time.now - ONE_DAY)
+          end
+
+          #
+          # Downloads the list file.
+          #
+          # @param [String] url
+          #   An optional alternate URL to download the `ip2asn-combined.tsv.gz`
+          #   file.
+          #
+          # @param [String] path
+          #   An optional alternate path to the list file.
+          #
+          def self.download(url: URL, path: PATH)
+            uri = URI(url)
+
+            Net::HTTP.start(uri.host,uri.port, use_ssl: true) do |http|
+              request  = Net::HTTP::Get.new(uri.path)
+
+              http.request(request) do |response|
+                FileUtils.mkdir_p(File.dirname(path))
+
+                File.open("#{path}.part",'wb') do |file|
+                  response.read_body do |chunk|
+                    file.write(chunk)
+                  end
+                end
+
+                FileUtils.mv("#{path}.part",path)
+              end
+            end
+          end
+
+          #
+          # Optionally update the cached list file if it is older than one day.
+          #
+          # @param [String] url
+          #   An optional alternate URL to download the `ip2asn-combined.tsv.gz`
+          #   file.
+          #
+          # @param [String] path
+          #   An optional alternate path to the list file.
+          #
+          def self.update(url: URL, path: PATH)
+            if !downloaded?(path)
+              download(url: url, path: path)
+            elsif stale?(path)
+              begin
+                download(url: url, path: path)
+              rescue
+              end
+            end
+          end
+
+          #
+          # Loads the TLD list from the given file.
+          #
+          # @param [String] path
+          #   The path to the TLD list file.
+          #
+          # @return [List]
+          #   The parsed TLD list file.
+          #
+          def self.load_file(path=PATH)
+            list = new(path)
+
+            File.open(path) do |file|
+              file.each_line(chomp: true) do |line|
+                next if line.start_with?('#')
+
+                list << line.downcase
+              end
+            end
+
+            return list
+          end
+
+          #
+          # Adds a TLD to the list.
+          #
+          # @param [String] tld
+          #   The TLD String to add.
+          #
+          # @return [self]
+          #
+          # @api private
+          #
+          def <<(tld)
+            @list << tld
+            return self
+          end
+
+          #
+          # Enumerates over each suffix in the list.
+          #
+          # @yield [suffix]
+          #   If a block is given, it will be passed each suffix in the list.
+          #
+          # @yieldparam [String] suffix
+          #   A domain suffix in the list.
+          #
+          # @return [Enumerator]
+          #   If no block is given, an Enumerator object will be returned.
+          #
+          def each(&block)
+            @list.each(&block)
+          end
+
+          #
+          # Creates a regular expression that can match every domain suffix in
+          # the list.
+          #
+          # @return [Regexp]
+          #
+          def to_regexp
+            regexp = Regexp.union(@list)
+
+            return /(?<=[^a-zA-Z0-9_-]|^)#{regexp}(?=[^\.a-z0-9-]|$)/
+          end
+
+          #
+          # Inspects the TLD list.
+          #
+          # @return [String]
+          #   The inspected list object.
+          #
+          def inspect
+            "#<#{self.class}: #{@path}>"
+          end
+
+        end
+      end
+    end
+  end
+end
---
+++ spec/network/tld/list_spec.rb
@@ -0,0 +1,361 @@
+require 'spec_helper'
+require 'ronin/support/network/tld/list'
+
+require 'fileutils'
+require 'tempfile'
+require 'tmpdir'
+
+describe Ronin::Support::Network::TLD::List do
+  let(:fixtures_dir) { File.join(__dir__,'fixtures') }
+  let(:list_file)    { File.join(fixtures_dir,'tlds-alpha-by-domain.txt') }
+  let(:path)         { list_file }
+
+  describe "FILE_NAME" do
+    subject { described_class::FILE_NAME }
+
+    it do
+      expect(subject).to eq('tlds-alpha-by-domain.txt')
+    end
+  end
+
+  describe "URL" do
+    subject { described_class::URL }
+
+    it do
+      expect(subject).to eq('https://data.iana.org/TLD/tlds-alpha-by-domain.txt')
+    end
+  end
+
+  describe "PATH" do
+    subject { described_class::PATH }
+
+    it do
+      expect(subject).to eq(File.join(Gem.user_home,'.cache','ronin','ronin-support','tlds-alpha-by-domain.txt'))
+    end
+  end
+
+  describe "file methods" do
+    let(:tempdir) { Dir.mktmpdir('ronin-support') }
+    let(:path)    { File.join(tempdir,described_class::FILE_NAME) }
+
+    before do
+      stub_const("#{described_class}::PATH",path)
+    end
+
+    describe ".downloaded?" do
+      subject { described_class }
+
+      context "when given no argument" do
+        context "when the file does not exist" do
+          it "must return false" do
+            expect(subject.downloaded?).to be(false)
+          end
+        end
+
+        context "when the file does exist" do
+          before { FileUtils.touch(path) }
+
+          it "must return true" do
+            expect(subject.downloaded?).to be(true)
+          end
+
+          after { FileUtils.rm(path) }
+        end
+      end
+
+      context "when given a path" do
+        context "when the file does not exist" do
+          it "must return false" do
+            expect(subject.downloaded?(path)).to be(false)
+          end
+        end
+
+        context "when the file does exist" do
+          before { FileUtils.touch(path) }
+
+          it "must return true" do
+            expect(subject.downloaded?(path)).to be(true)
+          end
+
+          after { FileUtils.rm(path) }
+        end
+      end
+    end
+
+    describe ".stale?" do
+      subject { described_class }
+
+      context "when given no argument" do
+        context "when the file does not exist" do
+          it "must return true" do
+            expect(subject.stale?).to be(true)
+          end
+        end
+
+        context "when the file does exist" do
+          before { FileUtils.touch(path) }
+
+          context "and the modification time of the file is within the last day" do
+            before do
+              FileUtils.touch(path, mtime: (Time.now - (60 * 60)))
+            end
+
+            it "must return false" do
+              expect(subject.stale?).to be(false)
+            end
+          end
+
+          context "but the modification time is older than one day" do
+            before do
+              FileUtils.touch(path, mtime: (Time.now - (60 * 60 * 25)))
+            end
+
+            it "must return true" do
+              expect(subject.stale?).to be(true)
+            end
+          end
+
+          after { FileUtils.rm(path) }
+        end
+      end
+
+      context "when given a path" do
+        context "when the file does not exist" do
+          it "must return true" do
+            expect(subject.stale?(path)).to be(true)
+          end
+        end
+
+        context "when the file does exist" do
+          before { FileUtils.touch(path) }
+
+          context "and the modification time of the file is within the last day" do
+            before do
+              FileUtils.touch(path, mtime: (Time.now - (60 * 60)))
+            end
+
+            it "must return false" do
+              expect(subject.stale?(path)).to be(false)
+            end
+          end
+
+          context "but the modification time is older than one day" do
+            before do
+              FileUtils.touch(path, mtime: (Time.now - (60 * 60 * 25)))
+            end
+
+            it "must return true" do
+              expect(subject.stale?(path)).to be(true)
+            end
+          end
+
+          after { FileUtils.rm(path) }
+        end
+      end
+    end
+
+    describe ".download", :network do
+      subject { described_class }
+
+      context "when given no keyword arguments" do
+        before { subject.download }
+
+        it "must download the URL to the PATH" do
+          expect(File.file?(described_class::PATH)).to be(true)
+          expect(File.stat(described_class::PATH).size).to be > 0
+        end
+      end
+
+      context "when the path: keyword argument is given" do
+        let(:tempfile) { Tempfile.new(['list', '.dat']) }
+        let(:path)     { tempfile.path }
+
+        before { subject.download(path: path) }
+
+        it "must download the ASN list to the specified path" do
+          expect(File.file?(path)).to be(true)
+          expect(File.stat(path).size).to be > 0
+        end
+
+        context "but the parent directory does not yet exist" do
+          let(:tempdir) { Dir.mktmpdir('ronin-support') }
+          let(:path)    { File.join(tempdir,'new_dir','list.dat') }
+
+          it "must create the parent directory" do
+            expect(File.directory?(File.dirname(path))).to be(true)
+            expect(File.file?(path)).to be(true)
+            expect(File.stat(path).size).to be > 0
+          end
+        end
+      end
+    end
+
+    describe ".update" do
+      subject { described_class }
+
+      context "when given no keyword arguments" do
+        context "and when .downloaded? returns false" do
+          before { allow(subject).to receive(:downloaded?).and_return(false) }
+
+          it "must call .download" do
+            expect(subject).to receive(:download)
+
+            subject.update
+          end
+
+          context "but when .download raises an exception" do
+            let(:exception) { RuntimeError.new("download failed") }
+
+            it "must ignore any exceptions" do
+              allow(subject).to receive(:download).and_raise(exception)
+
+              expect {
+                subject.update
+              }.to raise_error(exception)
+            end
+          end
+        end
+
+        context "and when .downloaded? returns true" do
+          before { allow(subject).to receive(:downloaded?).and_return(true) }
+
+          context "and when .stale? returns true" do
+            before { allow(subject).to receive(:stale?).and_return(true) }
+
+            it "must call .download" do
+              expect(subject).to receive(:download)
+
+              subject.update
+            end
+
+            context "but when .download raises an exception" do
+              it "must ignore any exceptions" do
+                allow(subject).to receive(:download).and_raise(
+                  RuntimeError.new("download failed")
+                )
+
+                expect {
+                  subject.update
+                }.to_not raise_error
+              end
+            end
+          end
+
+          context "and when .stale? returns false" do
+            before { allow(subject).to receive(:stale?).and_return(false) }
+
+            it "must not call .download" do
+              expect(subject).to_not receive(:download)
+
+              subject.update
+            end
+          end
+        end
+      end
+    end
+
+    describe ".load_file" do
+      let(:path) { list_file }
+
+      subject { described_class }
+
+      context "when given no argument" do
+        let(:path) { File.join(fixtures_dir,'tlds-alpha-by-domain.txt') }
+
+        subject { super().load_file }
+
+        it "must return a #{described_class} object with the parsed records" do
+          expect(subject).to be_kind_of(described_class)
+          expect(subject.list).to_not be_empty
+        end
+
+        it "must downcase the TLDs and not parse comment lines" do
+          expect(subject.list).to all(match(/\A(?:[a-z]+|xn--[a-z0-9-]+)\z/))
+        end
+      end
+
+      context "when given a path" do
+        subject { super().load_file(path) }
+
+        it "must return a #{described_class} object with the parsed records" do
+          expect(subject).to be_kind_of(described_class)
+          expect(subject.list).to_not be_empty
+        end
+
+        it "must downcase the TLDs and not parse comment lines" do
+          expect(subject.list).to all(match(/\A(?:[a-z]+|xn--[a-z0-9-]+)\z/))
+        end
+      end
+    end
+  end
+
+  describe "#initialize" do
+    subject { described_class.new }
+
+    it "must default #path to #{described_class}::PATH" do
+      expect(subject.path).to eq(described_class::PATH)
+    end
+
+    context "when given a custom path" do
+      let(:path) { '/path/to/tlds-alpha-by-domain.txt' }
+
+      subject { described_class.new(path) }
+
+      it "must set #path" do
+        expect(subject.path).to eq(path)
+      end
+    end
+
+    it "must initializes #list to []" do
+      expect(subject.list).to eq([])
+    end
+  end
+
+  describe "#<<" do
+    subject { described_class.new }
+
+    let(:tld1) { 'arpa' }
+    let(:tld2) { 'art'  }
+
+    it "must append the tld to the #list" do
+      subject << tld1
+      subject << tld2
+
+      expect(subject.list).to eq([tld1, tld2])
+    end
+  end
+
+  subject { described_class.load_file(path) }
+
+  describe "#each" do
+    context "when given a block" do
+      it "must yield every entry in #list" do
+        expect { |b|
+          subject.each(&b)
+        }.to yield_successive_args(*subject.list)
+      end
+    end
+
+    context "when given no block" do
+      it "must return an Enumerator for #each" do
+        expect(subject.each.to_a).to eq(subject.list)
+      end
+    end
+  end
+
+  describe "#to_regexp" do
+    it "must return a Regexp" do
+      expect(subject.to_regexp).to be_kind_of(Regexp)
+    end
+
+    it "must match every TLD in the #list" do
+      expect(subject.list).to all(match(subject.to_regexp))
+    end
+  end
+
+  describe "#inspect" do
+    it "must include the class name and the #path" do
+      expect(subject.inspect).to eq("#<#{described_class}: #{subject.path}>")
+    end
+  end
+end
