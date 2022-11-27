d9b9555483564106b75be63a3aa37f02795b1b78
Postmodern <postmodern.mod3@gmail.com>
2022-11-27 14:34:41 -0800

    Added `Network::TLD.list` (issue #385).

---
+++ spec/network/tld_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+require 'ronin/support/network/tld'
+
+describe Ronin::Support::Network::TLD do
+  describe ".list" do
+    let(:fixtures_dir) { File.join(__dir__,'tld','fixtures')    }
+    let(:list_file)    { File.join(fixtures_dir,'tlds-alpha-by-domain.txt') }
+
+    it "must call #{described_class}::List.update and #{described_class}::List.load_file" do
+      expect(described_class::List).to receive(:update)
+      expect(described_class::List).to receive(:load_file)
+
+      subject.list
+    end
+
+    it "must return a #{described_class}::List object" do
+      stub_const("#{described_class}::List::PATH",list_file)
+      allow(described_class::List).to receive(:update)
+
+      expect(subject.list).to be_kind_of(described_class::List)
+    end
+
+    it "must cache the loaded #{described_class}::List object" do
+      stub_const("#{described_class}::List::PATH",list_file)
+      allow(described_class::List).to receive(:update)
+
+      expect(subject.list).to be(subject.list)
+    end
+  end
+end
