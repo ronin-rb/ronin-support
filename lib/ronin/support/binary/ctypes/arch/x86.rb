7d85ed954608f3f4e9e4c05673a9b16ea2e844d6
Postmodern <postmodern.mod3@gmail.com>
2023-02-26 22:53:45 -0800

    Added a top-level description to `Binary::CTypes::Arch::X86`.

--- lib/ronin/support/binary/ctypes/arch/x86.rb
+++ lib/ronin/support/binary/ctypes/arch/x86.rb
@@ -23,6 +23,9 @@ module Ronin
     module Binary
       module CTypes
         module Arch
+          #
+          # Represents the C types for the x86 architecture.
+          #
           module X86
             include LittleEndian
 
