31de1f4126e51285de41df4aee1e40c5868c4c91
Gustavo Villa <gfvcastro@gmail.com>
2023-05-30 09:09:14 -0300

    Add `Ronin::Support::Binary::CTypes::OS::AppleIOS` class

---
+++ lib/ronin/support/binary/ctypes/os/apple_ios.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+#
+# Copyright (c) 2006-2023 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-support is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-support is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-support.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/support/binary/ctypes/os/bsd'
+
+module Ronin
+  module Support
+    module Binary
+      module CTypes
+        class OS
+          #
+          # Contains additional types available on Apple iOS.
+          #
+          # @api semipublic
+          #
+          # @since 1.1.0
+          #
+          class AppleIOS < BSD
+          end
+        end
+      end
+    end
+  end
+end
