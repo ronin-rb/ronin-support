e37d36e33afcf955cdda8584cdc06dd8cbf11af3
Postmodern <postmodern.mod3@gmail.com>
2024-08-09 01:53:57 -0700

    Added `Ronin::Support::Crypto::Cipher::DES3` (closes #524).

---
+++ spec/crypto/cipher/des3_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+require 'ronin/support/crypto/cipher/des3'
+
+describe Ronin::Support::Crypto::Cipher::DES3 do
+  let(:key) { 'A' * 24 }
+
+  describe "#initialize" do
+    subject do
+      described_class.new(
+        direction: :encrypt,
+        key:       key
+      )
+    end
+
+    it "must default #mode to nil" do
+      expect(subject.mode).to be(nil)
+    end
+
+    it "must initialize an DES3 cipher" do
+      expect(subject.name).to eq("DES-EDE3-CBC")
+    end
+
+    context "when given the mode: keyword argument" do
+      let(:mode) { :wrap }
+      let(:key)  { "A" * 24 }
+
+      subject do
+        described_class.new(
+          direction: :encrypt,
+          mode:      mode,
+          key:       key
+        )
+      end
+
+      it "must set #mode" do
+        expect(subject.mode).to eq(mode)
+      end
+
+      it "must initialize an DES3 cipher with the mode and direction" do
+        expect(subject.name).to eq("DES3-#{mode.upcase}")
+      end
+    end
+  end
+
+  describe ".supported" do
+    subject { described_class }
+
+    it "must return all ciphers beginning with 'des3'" do
+      expect(subject.supported).to_not be_empty
+      expect(subject.supported).to all(be =~ /^des3/)
+    end
+  end
+end
