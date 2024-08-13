72e5c978db81010fb9c6d00f2e081b1b5c8d519f
Postmodern <postmodern.mod3@gmail.com>
2024-08-12 17:52:59 -0700

    Make rubocop happy.

--- lib/ronin/support/encoding/shell.rb
+++ lib/ronin/support/encoding/shell.rb
@@ -262,7 +262,7 @@ module Ronin
           if data =~ /[^[:print:]]/
             "$'#{escape(data)}'"
           else
-            "\"#{escape(data).gsub('$',"\\$")}\""
+            "\"#{escape(data).gsub('$','\$')}\""
           end
         end
 
