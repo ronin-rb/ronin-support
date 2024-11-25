ab5c5d8589020f4d6805679211194b73de3f92cf
Postmodern <postmodern.mod3@gmail.com>
2024-11-24 20:48:04 -0800

    Added `String#defang` (closes #549).

--- spec/network/defang/core_ext/string_spec.rb
+++ spec/network/defang/core_ext/string_spec.rb
@@ -2,6 +2,48 @@ require 'spec_helper'
 require 'ronin/support/network/defang/core_ext/string'
 
 describe String do
+  describe "#defang" do
+    context "when given a defanged URL" do
+      subject { 'http://www.example.com/foo?q=1' }
+
+      let(:defanged) { 'hxxp[://]www[.]example[.]com/foo?q=1' }
+
+      it "must return the defanged URL" do
+        expect(subject.defang).to eq(defanged)
+      end
+    end
+
+    context "when given a defanged IPv4 address" do
+      subject { '192.168.1.1' }
+
+      let(:defanged) { '192[.]168[.]1[.]1' }
+
+      it "must return the defanged IPv4 address" do
+        expect(subject.defang).to eq(defanged)
+      end
+    end
+
+    context "when given a defanged IPv6 address" do
+      subject { '2606:2800:21f:cb07:6820:80da:af6b:8b2c' }
+
+      let(:defanged) { '2606[:]2800[:]21f[:]cb07[:]6820[:]80da[:]af6b[:]8b2c' }
+
+      it "must return the defanged IPv6 address" do
+        expect(subject.defang).to eq(defanged)
+      end
+    end
+
+    context "when given a defanged host name" do
+      subject { 'www.example.com' }
+
+      let(:defanged) { 'www[.]example[.]com' }
+
+      it "must return the defanged host name" do
+        expect(subject.defang).to eq(defanged)
+      end
+    end
+  end
+
   describe "#refang" do
     context "when the String is a defanged URL" do
       subject { 'hxxp[://]www[.]example[.]com/foo?q=1' }
