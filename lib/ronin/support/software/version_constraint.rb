31b6b7d1f162400f3c145468e8e6361905e51dd2
Postmodern <postmodern.mod3@gmail.com>
2024-11-27 19:05:07 -0800

    [WIP] Added `Software::VersionConstraint` (issue #551).

---
+++ lib/ronin/support/software/version_constraint.rb
@@ -0,0 +1,134 @@
+# frozen_string_literal: true
+#
+# Copyright (c) 2006-2024 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-support is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-support is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-support.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require_relative 'version'
+
+module Ronin
+  module Support
+    module Software
+      #
+      # Represents a version constraint (ex: `>= 1.2.3`).
+      #
+      # ## Examples
+      #
+      #     constraint = Software::VersionConstraint.new('>= 1.2.3')
+      #     version    = Software::Version.new('1.3.0')
+      #     constraint.include?(version)
+      #     # => true
+      #
+      # @since 1.2.0
+      #
+      class VersionConstraint
+
+        # The version constraint operator.
+        #
+        # @return [">", ">=", "<", "<=", "="]
+        #
+        # @api public
+        attr_reader :operator
+
+        # The parsed version number.
+        #
+        # @return [Version]
+        #
+        # @api public
+        attr_reader :version
+
+        #
+        # Initializes the version constraint.
+        #
+        # @param [String] string
+        #   The version constraint to parse.
+        #
+        # @raise [ArgumentError]
+        #   Could not parse the version constraint.
+        #
+        # @example
+        #   Software::VersionConstraint.new('>= 1.2.3')
+        #   Software::VersionConstraint.new('> 1.2.3')
+        #   Software::VersionConstraint.new('<= 1.2.3')
+        #   Software::VersionConstraint.new('< 1.2.3')
+        #   Software::VersionConstraint.new('= 1.2.3')
+        #   Software::VersionConstraint.new('1.2.3')
+        #
+        def initialize(string)
+          @string = string
+
+          if (match = constraint.match(/\A(?:(?<operator>>=|>|<=|<|=)?\s*)(?<version>.+)\z/))
+            @operator = match[:operator] || '='
+            @version  = match[:version]
+          else
+            raise(ArgumentError,"invalid version constraint: #{constraint.inspect}")
+          end
+        end
+
+        #
+        # Parses the version constraint.
+        #
+        # @param [String] string
+        #   The version constraint to parse.
+        #
+        # @return [VersionConstraint]
+        #   The parsed version constraint.
+        #
+        def self.parse(string)
+          new(string)
+        end
+
+        #
+        # Compares the version to the version constraint.
+        #
+        # @param [Version] version
+        #   The version number to compare.
+        #
+        # @return [Boolean]
+        #
+        # @api public
+        #
+        def include?(version)
+          case @operator
+          when '>'  then @version >  version
+          when '>=' then @version >= version
+          when '<'  then @version <  version
+          when '<=' then @version <= version
+          when '='  then @version == version
+          end
+        end
+
+        alias === include?
+
+        #
+        # Compares the version constraint to another version constraint.
+        #
+        # @param [VersionConstraint] other
+        #   The other version constraint to compare against.
+        #
+        # @return [Boolean]
+        #
+        # @api public
+        #
+        def ==(other)
+          self.class == other.class &&
+            @operator == other.operator &&
+            @version  == other.version
+        end
+
+      end
+    end
+  end
+end
